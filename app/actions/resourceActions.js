7434be3bec94df7e36d19a002764d76457db46d1
Lauri Raivio <lauri.raivio@gmail.com>
2015-09-28 10:31:18 +0300

    Add actions and reducer for fetching one resource
    Refs #8.

--- app/actions/resourceActions.js
+++ app/actions/resourceActions.js
@@ -4,10 +4,38 @@ import {API_URL} from 'constants/AppConstants';
 import types from 'constants/ActionTypes';
 
 export default {
+  fetchResource,
+  fetchResourceStart,
+  fetchResourceSuccess,
   fetchResources,
   fetchResourcesStart,
   fetchResourcesSuccess,
 };
+
+function fetchResourceStart() {
+  return {
+    type: types.FETCH_RESOURCE_START,
+  };
+}
+
+function fetchResourceSuccess(resource) {
+  return {
+    type: types.FETCH_RESOURCE_SUCCESS,
+    payload: {resource},
+  };
+}
+
+function fetchResource(id) {
+  return (dispatch) => {
+    dispatch(fetchResourceStart());
+
+    return fetch(`${API_URL}/resource/${id}`)
+      .then((response) => response.json())
+      .then((json) => {
+        dispatch(fetchResourceSuccess(json));
+      });
+  };
+}
 
 function fetchResourcesStart() {
   return {
