66251f5db54111816455d8fd51a6b2dd8ef34240
Lauri Raivio <lauri.raivio@gmail.com>
2015-10-06 04:56:45 +0300

    Make dataReducer tests more generic
    Makes it easier to add new data fetches.

--- app/reducers/dataReducer.js
+++ app/reducers/dataReducer.js
@@ -2,19 +2,28 @@ import Immutable from 'seamless-immutable';
 
 import ActionTypes from 'constants/ActionTypes';
 
+export default {
+  dataReducer,
+  handleData,
+};
+
 const initialState = Immutable({
   resources: {},
   units: {},
 });
 
-export function dataReducer(state = initialState, action) {
+function handleData(state, action) {
+  const data = action.payload.entities;
+  return state.merge(data, { deep: true });
+}
+
+function dataReducer(state = initialState, action) {
   switch (action.type) {
 
   case ActionTypes.FETCH_RESOURCE_SUCCESS:
   case ActionTypes.FETCH_RESOURCES_SUCCESS:
   case ActionTypes.FETCH_UNITS_SUCCESS:
-    const data = action.payload.entities;
-    return state.merge(data, { deep: true });
+    return handleData(state, action);
 
   default:
     return state;
