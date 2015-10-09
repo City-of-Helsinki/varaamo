dacddce535974db8c2c01c02e5eaf2d8144bb833
Lauri Raivio <lauri.raivio@gmail.com>
2015-10-09 14:14:49 +0300

    Add no results found message to search page
    Refs #36.

--- app/components/search/__tests__/SearchResults.spec.js
+++ app/components/search/__tests__/SearchResults.spec.js
@@ -9,79 +9,97 @@ import Resource from 'fixtures/Resource';
 import Unit from 'fixtures/Unit';
 
 describe('Component: SearchResults', () => {
-  const unit = Unit.build();
-  const props = {
-    results: Immutable([
-      Resource.build({ unit: unit.id }),
-      Resource.build({ unit: 'unfetched-unit' }),
-    ]),
-    units: Immutable({
-      [unit.id]: unit,
-    }),
-  };
-  let tree;
+  describe('with results', () => {
+    const unit = Unit.build();
+    const props = {
+      results: Immutable([
+        Resource.build({ unit: unit.id }),
+        Resource.build({ unit: 'unfetched-unit' }),
+      ]),
+      units: Immutable({
+        [unit.id]: unit,
+      }),
+    };
+    let tree;
 
-  before(() => {
-    tree = sd.shallowRender(<SearchResults {...props} />);
-  });
+    before(() => {
+      tree = sd.shallowRender(<SearchResults {...props} />);
+    });
 
-  describe('basic rendering', () => {
     it('should render a Table component', () => {
       const tableTrees = tree.everySubTree('Table');
 
       expect(tableTrees.length).to.equal(1);
     });
-  });
 
-  describe('rendering table headers', () => {
-    let thTrees;
+    describe('rendering table headers', () => {
+      let thTrees;
 
-    before(() => {
-      thTrees = tree.everySubTree('th');
-    });
+      before(() => {
+        thTrees = tree.everySubTree('th');
+      });
 
-    it('should render 2 th elements', () => {
-      expect(thTrees.length).to.equal(2);
-    });
+      it('should render 2 th elements', () => {
+        expect(thTrees.length).to.equal(2);
+      });
 
-    it('first th element should contain text "Tila"', () => {
-      expect(thTrees[0].text()).to.equal('Tila');
-    });
+      it('first th element should contain text "Tila"', () => {
+        expect(thTrees[0].text()).to.equal('Tila');
+      });
 
-    it('second th element should contain text "Sijainti"', () => {
-      expect(thTrees[1].text()).to.equal('Sijainti');
-    });
-  });
-
-  describe('rendering individual results', () => {
-    let resultTrees;
-
-    before(() => {
-      resultTrees = tree.everySubTree('SearchResult');
-    });
-
-    it('should render a SearchResult for every result in props', () => {
-      expect(resultTrees.length).to.equal(props.results.length);
-    });
-
-    it('should pass result as a prop to SearchResult', () => {
-      resultTrees.forEach((resultTree, index) => {
-        const resultVdom = resultTree.getRenderOutput();
-
-        expect(resultVdom.props.result).to.deep.equal(props.results[index]);
+      it('second th element should contain text "Sijainti"', () => {
+        expect(thTrees[1].text()).to.equal('Sijainti');
       });
     });
 
-    it('should pass unit corresponding to result.unit as a prop to SearchResult', () => {
-      const resultVdom = resultTrees[0].getRenderOutput();
+    describe('rendering individual results', () => {
+      let resultTrees;
 
-      expect(resultVdom.props.unit).to.deep.equal(unit);
+      before(() => {
+        resultTrees = tree.everySubTree('SearchResult');
+      });
+
+      it('should render a SearchResult for every result in props', () => {
+        expect(resultTrees.length).to.equal(props.results.length);
+      });
+
+      it('should pass result as a prop to SearchResult', () => {
+        resultTrees.forEach((resultTree, index) => {
+          const resultVdom = resultTree.getRenderOutput();
+
+          expect(resultVdom.props.result).to.deep.equal(props.results[index]);
+        });
+      });
+
+      it('should pass unit corresponding to result.unit as a prop to SearchResult', () => {
+        const resultVdom = resultTrees[0].getRenderOutput();
+
+        expect(resultVdom.props.unit).to.deep.equal(unit);
+      });
+
+      it('should pass empty object as unit prop to SearchResult if unit is unfetched', () => {
+        const resultVdom = resultTrees[1].getRenderOutput();
+
+        expect(resultVdom.props.unit).to.deep.equal({});
+      });
+    });
+  });
+
+  describe('without results', () => {
+    const props = {
+      results: [],
+      units: {},
+    };
+    let tree;
+
+    before(() => {
+      tree = sd.shallowRender(<SearchResults {...props} />);
     });
 
-    it('should pass empty object as unit prop to SearchResult if unit is unfetched', () => {
-      const resultVdom = resultTrees[1].getRenderOutput();
+    it('should render a message telling no results were found', () => {
+      const expected = 'Yhtään hakutulosta ei löytynyt.';
 
-      expect(resultVdom.props.unit).to.deep.equal({});
+      expect(tree.textIn('p')).to.equal(expected);
     });
   });
 });
