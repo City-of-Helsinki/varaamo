defbf55092b2d5ab28953914c7eec3ab37762d2d
Lauri Raivio <lauri.raivio@gmail.com>
2015-10-13 03:14:33 +0300

    Use buildAPIUrl with all API actions
    Better to use the buildAPIUrl than to construct the url
    by hand with API_URL constant.

--- app/actions/purposeActions.js
+++ app/actions/purposeActions.js
@@ -1,10 +1,9 @@
 import { arrayOf } from 'normalizr';
 import { CALL_API } from 'redux-api-middleware';
 
-import { API_URL } from 'constants/AppConstants';
 import types from 'constants/ActionTypes';
 import { purposeSchema } from 'middleware/Schemas';
-import { createTransformFunction } from 'utils/APIUtils';
+import { buildAPIUrl, createTransformFunction } from 'utils/APIUtils';
 
 export default {
   fetchPurposes,
@@ -18,7 +17,7 @@ function fetchPurposes() {
         types.FETCH_PURPOSES_SUCCESS,
         types.FETCH_PURPOSES_ERROR,
       ],
-      endpoint: `${API_URL}/purpose`,
+      endpoint: buildAPIUrl('purpose'),
       method: 'GET',
       transform: createTransformFunction(arrayOf(purposeSchema)),
       bailout: (state) => {
--- app/actions/unitActions.js
+++ app/actions/unitActions.js
@@ -1,10 +1,9 @@
 import { arrayOf } from 'normalizr';
 import { CALL_API } from 'redux-api-middleware';
 
-import { API_URL } from 'constants/AppConstants';
 import types from 'constants/ActionTypes';
 import { unitSchema } from 'middleware/Schemas';
-import { createTransformFunction } from 'utils/APIUtils';
+import { buildAPIUrl, createTransformFunction } from 'utils/APIUtils';
 
 export default {
   fetchUnits,
@@ -18,7 +17,7 @@ function fetchUnits() {
         types.FETCH_UNITS_SUCCESS,
         types.FETCH_UNITS_ERROR,
       ],
-      endpoint: `${API_URL}/unit`,
+      endpoint: buildAPIUrl('unit'),
       method: 'GET',
       transform: createTransformFunction(arrayOf(unitSchema)),
       bailout: (state) => {
