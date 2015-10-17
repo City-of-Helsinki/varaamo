cc1062e505b1d405cae2eebcae317c77af74f4f0
Lauri Raivio <lauri.raivio@gmail.com>
2015-10-17 04:28:27 +0300

    Add utils for changing filters to fetch params
    For fetches the date in filters needs be changed to
    start and end values. Also unsupported filters should be
    dropped.
    Refs #56.

--- app/utils/SearchUtils.js
+++ app/utils/SearchUtils.js
@@ -1,10 +1,22 @@
 import _ from 'lodash';
 
 import { SUPPORTED_SEARCH_FILTERS } from 'constants/AppConstants';
+import { getDateStartAndEndTimes } from 'utils/TimeUtils';
 
 export default {
+  getFetchParamsFromFilters,
   pickSupportedFilters,
 };
+
+function getFetchParamsFromFilters(filters) {
+  const all = Object.assign(
+    {},
+    pickSupportedFilters(filters),
+    getDateStartAndEndTimes(filters.date)
+  );
+
+  return _.omit(all, 'date');
+}
 
 function pickSupportedFilters(filters) {
   return _.pick(filters, SUPPORTED_SEARCH_FILTERS);
