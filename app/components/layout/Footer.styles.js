12fb964f4ef817967ad6833dd8ea49680ae95c31
Lauri Raivio <lauri.raivio@gmail.com>
2015-10-20 00:35:23 +0300

    Add content to footer
    Refs #4.

--- app/components/layout/Footer.styles.js
+++ app/components/layout/Footer.styles.js
@@ -5,4 +5,27 @@ export default {
     backgroundColor: colors.blue,
     color: '#fff',
   },
+
+  brandLink: {
+    color: '#fff',
+    fontSize: '36px',
+    lineHeight: '80px',
+    marginBottom: '30px',
+    display: 'block',
+
+    '@media (min-width: 992px)': {
+      marginBottom: '0px',
+    },
+  },
+
+  logo: {
+    display: 'inline-block',
+    verticalAlign: 'top',
+    height: 80,
+    marginRight: 20,
+  },
+
+  link: {
+    color: '#ccc',
+  },
 };
