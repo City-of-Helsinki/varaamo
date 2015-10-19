97a1b23bf9d10177b7d55ad883a9bcc41d0e24c0
Lauri Raivio <lauri.raivio@gmail.com>
2015-10-19 14:29:48 +0300

    Persist auth data to localstorage
    Keeps user logged in even if browser tab is refreshed.
    Refs #59.

--- app/store/configureStore.js
+++ app/store/configureStore.js
@@ -1,12 +1,27 @@
 import createHistory from 'history/lib/createHashHistory';
+import _ from 'lodash';
 import { compose, createStore, applyMiddleware } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
 import { reduxReactRouter } from 'redux-router';
+import reduxLocalStoragePersistState, { mergePersistedState } from 'redux-localstorage';
+import adapter from 'redux-localstorage/lib/adapters/localStorage';
+import filter from 'redux-localstorage-filter';
+import Immutable from 'seamless-immutable';
 
 import getRoutes from 'app/routes';
 import rootReducer from 'reducers/index';
 
 let finalCreateStore;
+
+const finalReducer = compose(
+  mergePersistedState((initialState, persistedState) => {
+    return Immutable(_.merge({}, initialState, persistedState));
+  })
+)(rootReducer);
+
+const storage = compose(
+  filter('auth')
+)(adapter(window.localStorage));
 
 if (__DEVTOOLS__) {
   const { devTools, persistState } = require('redux-devtools');
@@ -17,18 +32,20 @@ if (__DEVTOOLS__) {
     applyMiddleware(apiMiddleware),
     reduxReactRouter({ getRoutes, createHistory }),
     applyMiddleware(loggerMiddleware),
+    reduxLocalStoragePersistState(storage),
     devTools(),
     persistState(window.location.href.match(/[?&]debug_session=([^&]+)\b/))
   )(createStore);
 } else {
   finalCreateStore = compose(
     applyMiddleware(apiMiddleware),
-    reduxReactRouter({ getRoutes, createHistory })
+    reduxReactRouter({ getRoutes, createHistory }),
+    reduxLocalStoragePersistState(storage)
   )(createStore);
 }
 
 export default function configureStore(initialState) {
-  const store = finalCreateStore(rootReducer, initialState);
+  const store = finalCreateStore(finalReducer, initialState);
 
   if (module.hot) {
     // Enable Webpack hot module replacement for reducers
