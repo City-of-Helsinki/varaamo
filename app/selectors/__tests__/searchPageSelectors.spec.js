80bd79cc97c741b47237d46284b2a77a38ac69fb
Lauri Raivio <lauri.raivio@gmail.com>
2015-10-23 23:07:07 +0300

    Move searchResultsSelector to own file
    Refs #81.

--- app/selectors/__tests__/searchPageSelectors.spec.js
+++ app/selectors/__tests__/searchPageSelectors.spec.js
@@ -1,24 +1,16 @@
 import { expect } from 'chai';
 
-import _ from 'lodash';
 import Immutable from 'seamless-immutable';
 
 import types from 'constants/ActionTypes';
-import Resource from 'fixtures/Resource';
 import Unit from 'fixtures/Unit';
 import { searchPageSelectors } from 'selectors/searchPageSelectors';
 
 describe('Selectors: searchPageSelectors', () => {
   let unit;
-  let resources;
   let state;
 
   beforeEach(() => {
-    resources = [
-      Resource.build(),
-      Resource.build(),
-    ];
-
     unit = Unit.build();
 
     state = {
@@ -26,10 +18,7 @@ describe('Selectors: searchPageSelectors', () => {
         activeRequests: [],
       }),
       data: Immutable({
-        resources: {
-          [resources[0].id]: resources[0],
-          [resources[1].id]: resources[1],
-        },
+        resources: {},
         units: {
           [unit.id]: unit,
         },
@@ -40,7 +29,7 @@ describe('Selectors: searchPageSelectors', () => {
             date: '2015-10-10',
             purpose: 'some-purpose',
           },
-          results: [resources[0].id, resources[1].id],
+          results: [],
         },
       }),
     };
@@ -67,21 +56,10 @@ describe('Selectors: searchPageSelectors', () => {
     });
   });
 
-  it('should return resources corresponding to searchResults.ids as results', () => {
+  it('should return results', () => {
     const selected = searchPageSelectors(state);
-    const expected = [resources[0], resources[1]];
 
-    expect(selected.results).to.deep.equal(expected);
-  });
-
-  it('should return the results in alphabetical order', () => {
-    const unsortedResults = [resources[1].id, resources[0].id];
-    const unSortedState = _.merge({}, state, { ui: { search: { results: unsortedResults } } });
-
-    const selected = searchPageSelectors(unSortedState);
-    const expected = [resources[0], resources[1]];
-
-    expect(selected.results).to.deep.equal(expected);
+    expect(selected.results).to.exist;
   });
 
   it('should return units from the state', () => {
--- app/selectors/searchPageSelectors.js
+++ app/selectors/searchPageSelectors.js
@@ -3,35 +3,28 @@ import { createSelector } from 'reselect';
 
 import types from 'constants/ActionTypes';
 import searchFiltersSelector from 'selectors/searchFiltersSelector';
+import searchResultsSelector from 'selectors/searchResultsSelector';
 
 const activeRequestsSelector = (state) => state.api.activeRequests;
-const resourcesSelector = (state) => state.data.resources;
-const searchResultsSelector = (state) => state.ui.search.results;
 const unitsSelector = (state) => state.data.units;
 
 export const searchPageSelectors = createSelector(
   activeRequestsSelector,
-  resourcesSelector,
   searchFiltersSelector,
   searchResultsSelector,
   unitsSelector,
   (
     activeRequests,
-    resources,
     searchFilters,
     searchResults,
     units
   ) => {
     const isFetchingSearchResults = _.includes(activeRequests, types.API.RESOURCES_GET_REQUEST);
-    const results = _.sortBy(
-      searchResults.map(resourceId => resources[resourceId]),
-      (result) => result.name.fi
-    );
 
     return {
       filters: searchFilters,
       isFetchingSearchResults,
-      results,
+      results: searchResults,
       units,
     };
   }
