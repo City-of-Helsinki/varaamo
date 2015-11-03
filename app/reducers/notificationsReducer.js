3083b3098684156971f5bf354ec026bedaf1d6fe
Lauri Raivio <lauri.raivio@gmail.com>
2015-11-03 22:29:15 +0200

    Add notifications for user API interactions
    Refs #54.

--- app/reducers/notificationsReducer.js
+++ app/reducers/notificationsReducer.js
@@ -6,7 +6,12 @@ import { NOTIFICATION_DEFAULTS } from 'constants/AppConstants';
 const initialState = Immutable({});
 
 function addNotification(state, notification) {
-  return [...state, Object.assign({}, NOTIFICATION_DEFAULTS, notification)];
+  return [...state, Object.assign(
+    {},
+    NOTIFICATION_DEFAULTS,
+    notification,
+    { id: (state.length || 0) + 1 }
+  )];
 }
 
 function hideNotification(state, index) {
@@ -18,6 +23,8 @@ function hideNotification(state, index) {
 }
 
 function notificationsReducer(state = initialState, action) {
+  let message;
+  let notification;
   switch (action.type) {
 
   case types.UI.HIDE_NOTIFICATION:
@@ -25,9 +32,75 @@ function notificationsReducer(state = initialState, action) {
     return hideNotification(state, index);
 
   case types.API.LOGIN:
-    const notification = {
-      id: (state.length || 0) + 1,
-      message: 'Olet nyt kirjautunut sisään.',
+    notification = {
+      message: 'Sinut on nyt kirjattu sisään.',
+      type: 'success',
+    };
+    return addNotification(state, notification);
+
+  case types.API.LOGOUT:
+    notification = {
+      message: 'Sinut on nyt kirjattu ulos.',
+      type: 'success',
+    };
+    return addNotification(state, notification);
+
+  case types.API.RESERVATION_DELETE_ERROR:
+    message = 'Varauksen poistaminen epäonnistui. Yritä hetken kuluttua uudelleen.';
+    if (action.payload.status === 401) {
+      message = 'Sinulla ei ole oikeutta poistaa varausta.';
+    }
+
+    notification = {
+      message,
+      type: 'error',
+      timeOut: 10000,
+    };
+    return addNotification(state, notification);
+
+  case types.API.RESERVATION_DELETE_SUCCESS:
+    notification = {
+      message: 'Varauksen poistaminen onnistui.',
+      type: 'success',
+    };
+    return addNotification(state, notification);
+
+  case types.API.RESERVATION_POST_ERROR:
+    message = 'Varauksen tekeminen epäonnistui.';
+    if (action.payload.status === 401) {
+      message = 'Sinulla ei ole oikeutta tehdä varausta.';
+    }
+
+    notification = {
+      message,
+      type: 'error',
+      timeOut: 10000,
+    };
+    return addNotification(state, notification);
+
+  case types.API.RESERVATION_POST_SUCCESS:
+    notification = {
+      message: 'Varauksen tekeminen onnistui.',
+      type: 'success',
+    };
+    return addNotification(state, notification);
+
+  case types.API.RESERVATION_PUT_ERROR:
+    message = 'Varauksen muuttaminen epäonnistui.';
+    if (action.payload.status === 401) {
+      message = 'Sinulla ei ole oikeutta muuttaa varausta.';
+    }
+
+    notification = {
+      message,
+      type: 'error',
+      timeOut: 10000,
+    };
+    return addNotification(state, notification);
+
+  case types.API.RESERVATION_PUT_SUCCESS:
+    notification = {
+      message: 'Varauksen muuttaminen onnistui.',
       type: 'success',
     };
     return addNotification(state, notification);
