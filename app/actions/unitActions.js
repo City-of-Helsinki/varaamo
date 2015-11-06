513c2e1aeb8b82e2fc37cd06b458be198a65af62
Lauri Raivio <lauri.raivio@gmail.com>
2015-11-06 04:01:53 +0200

    Add Authorization header to API requests
    Adds the Authorization header with the proper
    authorization token when user is logged in.
    Refs #104.

--- app/actions/unitActions.js
+++ app/actions/unitActions.js
@@ -5,7 +5,7 @@ import { paginatedUnitsSchema } from 'middleware/Schemas';
 import {
   buildAPIUrl,
   getErrorTypeDescriptor,
-  getHeaders,
+  getHeadersCreator,
   getRequestTypeDescriptor,
   getSuccessTypeDescriptor,
 } from 'utils/APIUtils';
@@ -29,7 +29,7 @@ function fetchUnits() {
       ],
       endpoint: buildAPIUrl('unit', fetchParams),
       method: 'GET',
-      headers: getHeaders(),
+      headers: getHeadersCreator(),
       bailout: (state) => {
         return !state.api.shouldFetch.units;
       },
