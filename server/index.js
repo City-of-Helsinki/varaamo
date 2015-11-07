23efdc42b683380b831bb28e36420358b392f2d3
Lauri Raivio <lauri.raivio@gmail.com>
2015-11-08 00:14:16 +0200

    Remove unused global __DEVELOPMENT__ variable

--- server/index.js
+++ server/index.js
@@ -3,8 +3,6 @@ require('babel-core/register');
 
 var serverConfig = require('./config');
 
-global.__DEVELOPMENT__ = process.env.NODE_ENV !== 'production';
-
 // To ignore webpack custom loaders on server.
 serverConfig.webpackStylesExtensions.forEach(function(ext) {
   require.extensions['.' + ext] = function() {};
