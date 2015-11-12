e9b81941184536ced2b8484b28dfb899dae2d680
Lauri Raivio <lauri.raivio@gmail.com>
2015-11-12 07:13:26 +0200

    Add real content to about page
    Closes #143.

--- app/containers/AboutPage.js
+++ app/containers/AboutPage.js
@@ -6,23 +6,33 @@ class AboutPage extends Component {
     return (
       <DocumentTitle title="Tietoa palvelusta - Respa">
         <div className="about-page">
-          <h1>Tietoa Respa palvelusta</h1>
+          <h1>Tietoa respa.hel.fi –palvelusta</h1>
           <p className="lead">
-            Lorem ipsum dolor sit amet, summo audire feugait ei per. At mel movet facilisis
-            instructior. Qui ex hinc error probatus, nam eius repudiare reprimique et, cum an
-            dolore necessitatibus. Ad eam fabellas percipitur, viderer incorrupte philosophia pri
-            in.
+            Respa on Helsingin kaupungin ylläpitämä verkkopalvelu, jonka kautta voidaan varata
+            kaupungin julkisia tiloja sekä työpisteitä yksityiseen käyttöön.
           </p>
           <p>
-            Te suscipit patrioque necessitatibus est, eam harum habemus ex, vocent vituperata te
-            vix. Eam prompta perfecto similique id. At eos meis appareat, ad sed partem eirmod
-            interpretaris. Eum at dicam persius, menandri facilisis sed ad.
+            Palvelu on testausvaiheessa, ja sen pilottikäytössä olevassa versiossa ei ole vielä
+            kaikkia suunnitteilla olevia ominaisuuksia ja toiminnallisuuksia. Testiversion
+            tarkoituksena on kaupungin sisäisten tilavarauskäytäntöjen yhtenäistämisen ohella
+            saada palautetta myös itse palvelusta ja sen toiminnasta. <strong>Palautetta voi antaa
+            joka sivulla sijaitsevan palautelaatikon kautta. ?</strong>
           </p>
           <p>
-            Minimum fabellas scripserit eum in, in postulant aliquando eam. Erat nulla appareat id
-            sed. In verear nostrum nam, unum lobortis mel in. Mel ne quem omnes eruditi, nam
-            apeirian sententiae delicatissimi ex, usu et corpora accumsan tractatos. Mea timeam
-            delicatissimi ne. Dolor essent id has, vix sumo volumus principes ei.
+            Pilottivaiheessa palvelun kautta on varattavissa kaupunginkirjaston,
+            nuorisoasiainkeskuksen sekä varhaiskasvatusviraston tiloja ja työpisteitä. Osa palvelun
+            kautta tarjottavista tiloista on maksullisia ja niiden varaaminen suoraan
+            tilavarausjärjestelmän kautta ei ole alkuvaiheessa mahdollista, vaan maksullisista
+            tiloista nähtävissä ovat varauskalenteri sekä tarvittavat yhteystiedot tilavarauksen
+            tekemiseen.
+          </p>
+          <p>
+            Respa-tilavarausjärjestelmän kehittäminen toimii osana Helsingin kaupungin
+            strategiaohjelman 2013 - 2016 (Tehokkaat ja toimivat tukipalvelut) sekä
+            tietotekniikkaohjelman 2015 - 2017 (Datarajapinnat ja avoin kaupunkikehitys)
+            jalkauttamista. Virastoyhteisen tilavarauskokeilun tavoitteena on julkisten tilojen
+            käytön tehostaminen, saavutettavuuden parantaminen ja tilojen käyttöön liittyvien
+            kustannusten alentaminen.
           </p>
         </div>
       </DocumentTitle>
