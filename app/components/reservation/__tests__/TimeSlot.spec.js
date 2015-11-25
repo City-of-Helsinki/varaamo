f76da8fc18cbb580997480af86dcb66a88de3424
Lauri Raivio <lauri.raivio@gmail.com>
2015-11-25 15:52:55 +0200

    Refactor TimeSlot component tests
    Makes adding new tests and props easier.
    Refs #173.

--- app/components/reservation/__tests__/TimeSlot.spec.js
+++ app/components/reservation/__tests__/TimeSlot.spec.js
@@ -9,8 +9,8 @@ import TimeSlot from 'components/reservation/TimeSlot';
 import TimeSlotFixture from 'fixtures/TimeSlot';
 import Resource from 'fixtures/Resource';
 
-describe('Component: reservation/TimeSlot', () => {
-  const props = {
+function getProps(props) {
+  const defaults = {
     isAdmin: false,
     isLoggedIn: true,
     onClick: simple.stub(),
@@ -22,104 +22,105 @@ describe('Component: reservation/TimeSlot', () => {
     selectReservationToEdit: simple.stub(),
     slot: Immutable(TimeSlotFixture.build()),
   };
-  const tree = sd.shallowRender(<TimeSlot {...props} />);
-  const vdom = tree.getRenderOutput();
 
-  it('should render a table row', () => {
-    expect(vdom.type).to.equal('tr');
-  });
+  return Object.assign({}, defaults, props);
+}
 
-  it('clicking the table row should call props.onClick with correct arguments', () => {
-    tree.props.onClick();
+describe('Component: reservation/TimeSlot', () => {
+  describe('render', () => {
+    describe('when the slot is not reserved', () => {
+      const props = getProps();
+      const tree = sd.shallowRender(<TimeSlot {...props} />);
+      const vdom = tree.getRenderOutput();
 
-    expect(props.onClick.callCount).to.equal(1);
-  });
+      it('should render a table row', () => {
+        expect(vdom.type).to.equal('tr');
+      });
 
-  describe('table cells', () => {
-    const tdTrees = tree.everySubTree('td');
+      it('clicking the table row should call props.onClick with correct arguments', () => {
+        tree.props.onClick();
 
-    it('should render 3 table cells', () => {
-      expect(tdTrees).to.have.length(3);
+        expect(props.onClick.callCount).to.equal(1);
+      });
+
+      describe('table cells', () => {
+        const tdTrees = tree.everySubTree('td');
+
+        it('should render 3 table cells', () => {
+          expect(tdTrees).to.have.length(3);
+        });
+
+        describe('the first table cell', () => {
+          const tdTree = tdTrees[0];
+          const glyphiconTree = tdTree.subTree('Glyphicon');
+
+          it('should render a checkbox icon', () => {
+            expect(glyphiconTree).to.be.ok;
+          });
+
+          it('should not be checked if the slot is available', () => {
+            expect(glyphiconTree.props.glyph).to.equal('unchecked');
+          });
+        });
+
+        describe('the second table cell', () => {
+          const tdTree = tdTrees[1];
+
+          it('should display the slot time range as string', () => {
+            expect(tdTree.text()).to.equal(props.slot.asString);
+          });
+
+          it('should have the specific time range inside a "time" element', () => {
+            const timeTree = tdTree.subTree('time');
+            const expected = `${props.slot.start}/${props.slot.end}`;
+
+            expect(timeTree.props.dateTime).to.equal(expected);
+          });
+        });
+
+        describe('the third table cell', () => {
+          describe('when the slot is available', () => {
+            const tdTree = tdTrees[2];
+            const labelTrees = tdTree.everySubTree('Label');
+
+            it('should render a label', () => {
+              expect(labelTrees.length).to.equal(1);
+            });
+
+            it('should give proper props to the label', () => {
+              expect(labelTrees[0].props.bsStyle).to.equal('success');
+            });
+
+            it('should display text "Vapaa"', () => {
+              const expected = 'Vapaa';
+
+              expect(labelTrees[0].props.children).to.equal(expected);
+            });
+          });
+        });
+      });
     });
 
-    describe('the first table cell', () => {
-      const tdTree = tdTrees[0];
-      const glyphiconTree = tdTree.subTree('Glyphicon');
+    describe('when the slot is reserved', () => {
+      const props = getProps({
+        slot: Immutable(TimeSlotFixture.build({ reserved: true })),
+      });
+      const reservedTree = sd.shallowRender(<TimeSlot {...props} />);
+      const tdTree = reservedTree.everySubTree('td')[2];
+      const labelTrees = tdTree.everySubTree('Label');
 
-      it('should render a checkbox icon', () => {
-        expect(glyphiconTree).to.be.ok;
+      it('should render a label', () => {
+        expect(labelTrees.length).to.equal(1);
       });
 
-      it('should not be checked if the slot is available', () => {
-        expect(glyphiconTree.props.glyph).to.equal('unchecked');
-      });
-    });
-
-    describe('the second table cell', () => {
-      const tdTree = tdTrees[1];
-
-      it('should display the slot time range as string', () => {
-        expect(tdTree.text()).to.equal(props.slot.asString);
+      it('should give proper props to the label', () => {
+        expect(labelTrees[0].props.bsStyle).to.equal('danger');
       });
 
-      it('should have the specific time range inside a "time" element', () => {
-        const timeTree = tdTree.subTree('time');
-        const expected = `${props.slot.start}/${props.slot.end}`;
+      it('should display text "Varattu"', () => {
+        const expected = 'Varattu';
 
-        expect(timeTree.props.dateTime).to.equal(expected);
-      });
-    });
-
-    describe('the third table cell', () => {
-      describe('when the slot is available', () => {
-        const tdTree = tdTrees[2];
-        const labelTrees = tdTree.everySubTree('Label');
-
-        it('should render a label', () => {
-          expect(labelTrees.length).to.equal(1);
-        });
-
-        it('should give proper props to the label', () => {
-          expect(labelTrees[0].props.bsStyle).to.equal('success');
-        });
-
-        it('should display text "Vapaa"', () => {
-          const expected = 'Vapaa';
-
-          expect(labelTrees[0].props.children).to.equal(expected);
-        });
-      });
-
-      describe('when the slot is reserved', () => {
-        const reservedProps = {
-          isAdmin: false,
-          isLoggedIn: true,
-          onClick: simple.stub(),
-          openReservationDeleteModal: simple.stub(),
-          pushState: simple.stub(),
-          resource: Resource.build(),
-          selected: false,
-          selectReservationToDelete: simple.stub(),
-          selectReservationToEdit: simple.stub(),
-          slot: Immutable(TimeSlotFixture.build({ reserved: true })),
-        };
-        const reservedTree = sd.shallowRender(<TimeSlot {...reservedProps} />);
-        const tdTree = reservedTree.everySubTree('td')[2];
-        const labelTrees = tdTree.everySubTree('Label');
-
-        it('should render a label', () => {
-          expect(labelTrees.length).to.equal(1);
-        });
-
-        it('should give proper props to the label', () => {
-          expect(labelTrees[0].props.bsStyle).to.equal('danger');
-        });
-
-        it('should display text "Varattu"', () => {
-          const expected = 'Varattu';
-
-          expect(labelTrees[0].props.children).to.equal(expected);
-        });
+        expect(labelTrees[0].props.children).to.equal(expected);
       });
     });
   });
