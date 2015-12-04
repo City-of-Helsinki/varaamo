4f1f521393f81711f1b24be16eb22bd4fcfbf9a7
Lauri Raivio <lauri.raivio@gmail.com>
2015-12-04 15:03:26 +0200

    Add information about the service to home page
    Refs #218.

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -8,6 +8,12 @@ class HomePage extends Component {
     return (
       <DocumentTitle title="Etusivu - Varaamo">
         <div>
+          <h2>Varaa vaivatta kaupungin tiloja ja laitteita</h2>
+          <p>
+            Varaustilanteen näet kirjautumatta. Varaaminen edellyttää kirjautumista. Kyseessä on
+            kokeiluasteella oleva palvelu, jonka kautta varataan Kaupunginkirjaston,
+            Nuorisoasiainkeskuksen ja Varhaiskasvatusviraston tiloja ja työpisteitä.
+          </p>
           <h2>Mitä haluat tehdä?</h2>
           <PurposeCategoryList />
         </div>
