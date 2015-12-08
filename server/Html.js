f8bcbc45072990a0cb69aac921f1a0617f121ae6
Juha Yrjölä <juha.yrjola@iki.fi>
2015-12-08 10:12:27 +0200

    Fix Piwik site id

--- server/Html.js
+++ server/Html.js
@@ -18,7 +18,7 @@ class Html extends Component {
       (function() {
         var u="//analytics.hel.ninja/piwik/";
         _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', 4]);
+        _paq.push(['setSiteId', 5]);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
         g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
       })();
