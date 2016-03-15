e301050eb91b976efcf8a8f51bf25094f98db046
Lauri Raivio <lauri.raivio@gmail.com>
2016-03-15 18:40:48 +0200

    Add validation to preliminary reservation form
    Refs #245.

--- app/containers/PreliminaryReservationForm.js
+++ app/containers/PreliminaryReservationForm.js
@@ -1,52 +1,79 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Input from 'react-bootstrap/lib/Input';
+import { reduxForm } from 'redux-form';
 
-class PreliminaryReservationForm extends Component {
+import isEmail from 'validator/lib/isEmail';
+
+export function validate(values) {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = 'Pakollinen tieto';
+  }
+
+  if (!values.email) {
+    errors.email = 'Pakollinen tieto';
+  } else if (!isEmail(values.email)) {
+    errors.email = 'Syötä kunnollinen sähköpostiosoite';
+  }
+
+  if (!values.phone) {
+    errors.phone = 'Pakollinen tieto';
+  }
+
+  if (!values.description) {
+    errors.description = 'Pakollinen tieto';
+  }
+
+  if (!values.address) {
+    errors.address = 'Pakollinen tieto';
+  }
+  return errors;
+}
+
+export class UnconnectedPreliminaryReservationForm extends Component {
+  renderField(type, label, field, extraProps) {
+    const hasError = field.error && field.touched;
+    return (
+      <Input
+        {...field}
+        {...extraProps}
+        bsStyle={hasError ? 'error' : null}
+        help={hasError ? field.error : null}
+        label={label}
+        labelClassName="col-xs-3"
+        type={type}
+        wrapperClassName="col-xs-9"
+      />
+    );
+  }
+
   render() {
+    const { fields } = this.props;
     return (
       <div>
         <form className="form-horizontal">
           <p>
             Täytä vielä seuraavat tiedot alustavaa varausta varten.
-            Tähdellä (*) merkityt kentät ovat pakollisia.
+            Tähdellä (*) merkityt tiedot ovat pakollisia.
           </p>
-          <Input
-            label="Nimi*"
-            labelClassName="col-xs-3"
-            type="text"
-            wrapperClassName="col-xs-9"
-          />
-          <Input
-            label="Sähköposti*"
-            labelClassName="col-xs-3"
-            type="email"
-            wrapperClassName="col-xs-9"
-          />
-          <Input
-            label="Puhelin*"
-            labelClassName="col-xs-3"
-            type="text"
-            wrapperClassName="col-xs-9"
-          />
-          <Input
-            label="Tilaisuuden kuvaus*"
-            labelClassName="col-xs-3"
-            rows="5"
-            type="textarea"
-            wrapperClassName="col-xs-9"
-          />
-          <Input
-            label="Osoite*"
-            labelClassName="col-xs-3"
-            type="text"
-            wrapperClassName="col-xs-9"
-          />
+          {this.renderField('text', 'Nimi*', fields.name)}
+          {this.renderField('email', 'Sähköposti*', fields.email)}
+          {this.renderField('text', 'Puhelin*', fields.phone)}
+          {this.renderField('textarea', 'Tilaisuuden kuvaus*', fields.description, { rows: 5 })}
+          {this.renderField('text', 'Osoite*', fields.address)}
         </form>
       </div>
     );
   }
 }
 
-PreliminaryReservationForm.propTypes = {};
+UnconnectedPreliminaryReservationForm.propTypes = {
+  fields: PropTypes.object.isRequired,
+};
 
-export default PreliminaryReservationForm;
+export default reduxForm({
+  fields: ['name', 'email', 'phone', 'description', 'address'],
+  form: 'preliminaryReservation',
+  validate,
+})(UnconnectedPreliminaryReservationForm);
--- app/containers/__tests__/PreliminaryReservationForm.spec.js
+++ app/containers/__tests__/PreliminaryReservationForm.spec.js
@@ -4,57 +4,126 @@ import React from 'react';
 
 import Input from 'react-bootstrap/lib/Input';
 
-import PreliminaryReservationForm from 'containers/PreliminaryReservationForm';
+import {
+  UnconnectedPreliminaryReservationForm as PreliminaryReservationForm,
+  validate,
+} from 'containers/PreliminaryReservationForm';
 
 describe('Container: PreliminaryReservationForm', () => {
-  const props = {};
+  const fields = {
+    name: {},
+    email: {},
+    phone: {},
+    description: {},
+    address: {},
+  };
+  const props = {
+    fields,
+  };
   const wrapper = shallow(<PreliminaryReservationForm {...props} />);
 
-  it('should render a form', () => {
-    const form = wrapper.find('form');
-    expect(form.length).to.equal(1);
+  describe('validation', () => {
+    let validValues;
+
+    beforeEach(() => {
+      validValues = {
+        name: 'Luke',
+        email: 'luke@skywalker.com',
+        phone: '1234567',
+        description: 'Some description...',
+        address: 'Desert street 5, Mos Eisley',
+      };
+    });
+
+    it('should not return errors for valid values', () => {
+      const errors = validate(validValues);
+      expect(errors).to.deep.equal({});
+    });
+
+    it('should return an error if name is missing', () => {
+      validValues.name = undefined;
+      const errors = validate(validValues);
+      expect(errors.name).to.exist;
+    });
+
+    it('should return an error if email is missing', () => {
+      validValues.email = undefined;
+      const errors = validate(validValues);
+      expect(errors.email).to.exist;
+    });
+
+    it('should return an error if email is invalid', () => {
+      validValues.email = 'luke@';
+      const errors = validate(validValues);
+      expect(errors.email).to.exist;
+    });
+
+    it('should return an error if phone is missing', () => {
+      validValues.phone = undefined;
+      const errors = validate(validValues);
+      expect(errors.phone).to.exist;
+    });
+
+    it('should return an error if description is missing', () => {
+      validValues.description = undefined;
+      const errors = validate(validValues);
+      expect(errors.description).to.exist;
+    });
+
+    it('should return an error if address is missing', () => {
+      validValues.address = undefined;
+      const errors = validate(validValues);
+      expect(errors.address).to.exist;
+    });
   });
 
-  describe('form inputs', () => {
-    const inputs = wrapper.find(Input);
-
-    it('should render 5 inputs', () => {
-      expect(inputs.length).to.equal(5);
+  describe('rendering', () => {
+    it('should render a form', () => {
+      const form = wrapper.find('form');
+      expect(form.length).to.equal(1);
     });
 
-    it('should render a text input with label "Nimi*"', () => {
-      const nameInput = inputs.at(0);
+    describe('form inputs', () => {
+      const inputs = wrapper.find(Input);
 
-      expect(nameInput.props().type).to.equal('text');
-      expect(nameInput.props().label).to.equal('Nimi*');
-    });
+      it('should render 5 inputs', () => {
+        expect(inputs.length).to.equal(5);
+      });
 
-    it('should render a email input with label "Sähköposti*"', () => {
-      const nameInput = inputs.at(1);
+      it('should render a text input with label "Nimi*"', () => {
+        const nameInput = inputs.at(0);
 
-      expect(nameInput.props().type).to.equal('email');
-      expect(nameInput.props().label).to.equal('Sähköposti*');
-    });
+        expect(nameInput.props().type).to.equal('text');
+        expect(nameInput.props().label).to.equal('Nimi*');
+      });
 
-    it('should render a text input with label "Puhelin*"', () => {
-      const nameInput = inputs.at(2);
+      it('should render a email input with label "Sähköposti*"', () => {
+        const nameInput = inputs.at(1);
 
-      expect(nameInput.props().type).to.equal('text');
-      expect(nameInput.props().label).to.equal('Puhelin*');
-    });
+        expect(nameInput.props().type).to.equal('email');
+        expect(nameInput.props().label).to.equal('Sähköposti*');
+      });
 
-    it('should render a textarea input with label "Tilaisuuden kuvaus*"', () => {
-      const nameInput = inputs.at(3);
+      it('should render a text input with label "Puhelin*"', () => {
+        const nameInput = inputs.at(2);
 
-      expect(nameInput.props().type).to.equal('textarea');
-      expect(nameInput.props().label).to.equal('Tilaisuuden kuvaus*');
-    });
+        expect(nameInput.props().type).to.equal('text');
+        expect(nameInput.props().label).to.equal('Puhelin*');
+      });
 
-    it('should render a text input with label "Osoite*"', () => {
-      const nameInput = inputs.at(4);
+      it('should render a textarea input with label "Tilaisuuden kuvaus*"', () => {
+        const nameInput = inputs.at(3);
 
-      expect(nameInput.props().type).to.equal('text');
-      expect(nameInput.props().label).to.equal('Osoite*');
+        expect(nameInput.props().type).to.equal('textarea');
+        expect(nameInput.props().label).to.equal('Tilaisuuden kuvaus*');
+      });
+
+      it('should render a text input with label "Osoite*"', () => {
+        const nameInput = inputs.at(4);
+
+        expect(nameInput.props().type).to.equal('text');
+        expect(nameInput.props().label).to.equal('Osoite*');
+      });
     });
   });
 });
