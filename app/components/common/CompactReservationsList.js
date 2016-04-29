c21a96f331e6f26749ed18f9a46494f1d1871e15
Lauri Raivio <lauri.raivio@gmail.com>
2016-04-29 13:39:34 +0300

    Add CompactReservationsList component
    Makes displaying lists of reservations easier. Useful especially in
    all the reservation modals.
    Refs #282.

---
+++ app/components/common/CompactReservationsList.js
@@ -0,0 +1,38 @@
+import map from 'lodash/collection/map';
+import React, { Component, PropTypes } from 'react';
+
+import TimeRange from 'components/common/TimeRange';
+import { getName } from 'utils/DataUtils';
+
+class CompactReservationsList extends Component {
+  constructor(props) {
+    super(props);
+    this.renderReservation = this.renderReservation.bind(this);
+  }
+
+  renderReservation(reservation) {
+    const resource = this.props.resources[reservation.resource] || {};
+    return (
+      <li key={reservation.begin}>
+        {getName(resource)}
+        {': '}
+        <TimeRange begin={reservation.begin} end={reservation.end} />
+      </li>
+    );
+  }
+
+  render() {
+    return (
+      <ul>
+        {map(this.props.reservations, this.renderReservation)}
+      </ul>
+    );
+  }
+}
+
+CompactReservationsList.propTypes = {
+  reservations: PropTypes.array.isRequired,
+  resources: PropTypes.object.isRequired,
+};
+
+export default CompactReservationsList;
