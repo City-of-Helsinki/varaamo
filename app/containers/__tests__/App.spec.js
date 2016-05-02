5ba84af9510c1fabf2b821784d8ad20c81034d3a
Lauri Raivio <lauri.raivio@gmail.com>
2016-05-02 14:03:58 +0300

    Fetch logged in user data when app mounts
    Refs #292.

--- app/containers/__tests__/App.spec.js
+++ app/containers/__tests__/App.spec.js
@@ -1,51 +1,82 @@
 import { expect } from 'chai';
 import React from 'react';
-import sd from 'skin-deep';
+import { shallow } from 'enzyme';
 import simple from 'simple-mock';
 
 import Immutable from 'seamless-immutable';
 
+import Footer from 'components/layout/Footer';
+import Navbar from 'components/layout/Navbar';
 import { UnconnectedApp as App } from 'containers/App';
 import User from 'fixtures/User';
 
 describe('Container: App', () => {
-  const props = {
+  const user = User.build();
+  const defaultProps = {
     actions: {
       clearSearchResults: simple.stub(),
+      fetchUser: simple.stub(),
       updatePath: simple.stub(),
     },
     children: <div id="child-div" />,
     isLoggedIn: true,
-    user: Immutable(User.build()),
+    user: Immutable(user),
+    userId: user.id,
   };
-  const tree = sd.shallowRender(<App {...props} />);
 
-  describe('rendering Navbar', () => {
-    const navbarTrees = tree.everySubTree('Navbar');
+  function getWrapper(extraProps) {
+    return shallow(<App {...defaultProps} {...extraProps} />);
+  }
 
-    it('should render Navbar component', () => {
-      expect(navbarTrees.length).to.equal(1);
+  describe('rendering', () => {
+    const wrapper = getWrapper();
+
+    describe('Navbar', () => {
+      const navbar = wrapper.find(Navbar);
+
+      it('should render Navbar component', () => {
+        expect(navbar.length).to.equal(1);
+      });
+
+      it('should pass correct props to Navbar component', () => {
+        const actualProps = navbar.props();
+        expect(actualProps.clearSearchResults).to.equal(defaultProps.actions.clearSearchResults);
+        expect(actualProps.isLoggedIn).to.equal(defaultProps.isLoggedIn);
+        expect(actualProps.user).to.equal(defaultProps.user);
+      });
     });
 
-    it('should pass correct props to Navbar component', () => {
-      const actualProps = navbarTrees[0].props;
+    describe('props.children', () => {
+      it('should render props.children', () => {
+        const children = wrapper.find('#child-div');
+        expect(children.length).to.equal(1);
+      });
+    });
 
-      expect(actualProps.clearSearchResults).to.equal(props.actions.clearSearchResults);
-      expect(actualProps.isLoggedIn).to.equal(props.isLoggedIn);
-      expect(actualProps.user).to.equal(props.user);
+    it('should render Footer component', () => {
+      const footer = wrapper.find(Footer);
+      expect(footer.length).to.equal(1);
     });
   });
 
-  describe('rendering props.children', () => {
-    it('should render props.children', () => {
-      const childTrees = tree.everySubTree('#child-div');
-
-      expect(childTrees.length).to.equal(1);
+  describe('componentDidMount', () => {
+    describe('when user is not logged in', () => {
+      it('should not fetch user data', () => {
+        defaultProps.actions.fetchUser.reset();
+        const instance = getWrapper({ user: {}, userId: null }).instance();
+        instance.componentDidMount();
+        expect(defaultProps.actions.fetchUser.callCount).to.equal(0);
+      });
     });
-  });
 
-  it('should render Footer component', () => {
-    const footerTrees = tree.everySubTree('Footer');
-    expect(footerTrees.length).to.equal(1);
+    describe('when user is logged in', () => {
+      it('should fetch user data', () => {
+        defaultProps.actions.fetchUser.reset();
+        const instance = getWrapper().instance();
+        instance.componentDidMount();
+        expect(defaultProps.actions.fetchUser.callCount).to.equal(1);
+        expect(defaultProps.actions.fetchUser.lastCall.args[0]).to.equal(user.id);
+      });
+    });
   });
 });
--- app/selectors/containers/appSelector.js
+++ app/selectors/containers/appSelector.js
@@ -19,6 +19,7 @@ const appSelector = createSelector(
     return {
       isLoggedIn,
       user,
+      userId,
     };
   }
 );
