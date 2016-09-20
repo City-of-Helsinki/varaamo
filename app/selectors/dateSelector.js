fbfc84b4fd1b2895e12c653fcf771b8b22a496a4
Lauri Raivio <lauri.raivio@gmail.com>
2016-09-20 15:18:20 +0300

    Improve dateSelector to fall back on state
    Refs #368.

--- app/selectors/dateSelector.js
+++ app/selectors/dateSelector.js
@@ -1,5 +1,12 @@
+import queryString from 'query-string';
+
 import { getDateString } from 'utils/TimeUtils';
 
-const dateSelector = (state, props) => getDateString(props.location.query.date);
+const dateSelector = (state, props) => {
+  const query = props && props.location ?
+    props.location.query :
+    queryString.parse(queryString.extract(state.routing.path));
+  return getDateString(query.date);
+};
 
 export default dateSelector;
