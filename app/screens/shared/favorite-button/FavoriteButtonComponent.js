69b91e6bcec7ac9560258d3e33e2d53930aaec04
Raúl García <serulo@gmail.com>
2016-09-20 17:46:01 +0200

    Add FavoriteButtonComponent (Refs #347)

---
+++ app/screens/shared/favorite-button/FavoriteButtonComponent.js
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
+
+function FavoriteButtonComponent({ favorited, onClick }) {
+  return (
+    <button
+      className="favorite-button"
+      onClick={onClick}
+    >
+      { favorited ?
+        <Glyphicon glyph="star" /> :
+        <Glyphicon glyph="star-empty" />
+      }
+    </button>
+  );
+}
+
+FavoriteButtonComponent.propTypes = {
+  favorited: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default FavoriteButtonComponent;
