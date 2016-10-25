0887054e9634b789677aed19331258892bf8d9e5
Lauri Raivio <lauri.raivio@gmail.com>
2016-10-25 15:56:24 +0300

    Improve image carousel responsiveness
    Refs #475.

--- app/pages/resource/resource-info/ImageCarousel.js
+++ app/pages/resource/resource-info/ImageCarousel.js
@@ -1,21 +1,14 @@
 import React, { PropTypes } from 'react';
 import Carousel from 'react-bootstrap/lib/Carousel';
 
-const IMAGE_WIDTH = 700;
-const IMAGE_HEIGHT = 420;
+const carouselInterval = 7000;
 
 function renderCarouselItem(image) {
   return (
     <Carousel.Item key={image.url}>
       <div
-        style={{
-          backgroundColor: '#ccc',
-          backgroundImage: `url(${image.url})`,
-          backgroundPosition: '50% 50%',
-          backgroundSize: 'cover',
-          height: IMAGE_HEIGHT,
-          width: IMAGE_WIDTH,
-        }}
+        className="image-container"
+        style={{ backgroundImage: `url(${image.url})` }}
       />
     </Carousel.Item>
   );
@@ -27,8 +20,7 @@ function ImageCarousel({ images }) {
       className="image-carousel"
       controls={images.length > 1}
       indicators={images.length > 1}
-      interval={7000}
-      style={{ maxWidth: IMAGE_WIDTH }}
+      interval={carouselInterval}
     >
       {images.map(renderCarouselItem)}
     </Carousel>
