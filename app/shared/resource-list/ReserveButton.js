3a7f7ebbaaad5e6188e8e906b4c9fb1056560775
Lauri Raivio <lauri.raivio@gmail.com>
2016-10-31 12:43:23 +0200

    Show ReserveButton only when needed
    Removes ReserveButton when:
    - user is looking at past dates.
    - reservation is limited too far in the future.
    Refs #472.

--- app/shared/resource-list/ReserveButton.js
+++ app/shared/resource-list/ReserveButton.js
@@ -1,3 +1,4 @@
+import moment from 'moment';
 import React, { PropTypes } from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import { LinkContainer } from 'react-router-bootstrap';
@@ -5,6 +6,21 @@ import { LinkContainer } from 'react-router-bootstrap';
 import { getResourcePageUrl } from 'utils/resourceUtils';
 
 function ReserveButton({ date, isLoggedIn, resource }) {
+  const now = moment();
+  if (moment(date).isBefore(now, 'day')) {
+    return <span />;
+  }
+
+  const isAdmin = resource.userPermissions && resource.userPermissions.isAdmin;
+  const reservationLimited = (
+    resource.reservableBefore &&
+    moment(resource.reservableBefore).isBefore(moment(date), 'day')
+  );
+
+  if (reservationLimited && !isAdmin) {
+    return <span />;
+  }
+
   const isReservable = isLoggedIn && resource.reservable;
   let buttonText;
 
@@ -29,6 +45,7 @@ ReserveButton.propTypes = {
   resource: PropTypes.shape({
     needManualConfirmation: PropTypes.bool.isRequired,
     reservable: PropTypes.bool.isRequired,
+    userPermissions: PropTypes.object.isRequired,
   }).isRequired,
 };
 
