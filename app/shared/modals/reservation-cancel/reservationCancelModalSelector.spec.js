76e13227a08cfeb4fb15b36ddd7afb7e26f81525
Lauri Raivio <lauri.raivio@gmail.com>
2016-12-21 15:34:53 +0200

    Refactor and add translations to cancel modal
    Refs #478.

--- app/shared/modals/reservation-cancel/reservationCancelModalSelector.spec.js
+++ app/shared/modals/reservation-cancel/reservationCancelModalSelector.spec.js
@@ -11,8 +11,42 @@ describe('shared/modals/reservation-cancel/reservationCancelModalSelector', () =
     return reservationCancelModalSelector(state);
   }
 
-  it('returns isAdmin', () => {
-    expect(getSelected().isAdmin).to.exist;
+  describe('cancelAllowed', () => {
+    it('returns true if user is an admin', () => {
+      const selected = getSelected({
+        auth: { userId: 'u-1' },
+        'data.users': { 'u-1': { isStaff: true } },
+      });
+      expect(selected.cancelAllowed).to.be.true;
+    });
+
+    it('returns true if reservation is not preliminary', () => {
+      const reservation = { id: 'reservation-1', needManualConfirmation: false };
+      const selected = getSelected({
+        'ui.reservations.toCancel': [reservation],
+      });
+      expect(selected.cancelAllowed).to.be.true;
+    });
+
+    it('returns true if reservation state is not confirmed', () => {
+      const reservation = { id: 'reservation-1', state: 'requested' };
+      const selected = getSelected({
+        'ui.reservations.toCancel': [reservation],
+      });
+      expect(selected.cancelAllowed).to.be.true;
+    });
+
+    it('returns false if user is not admin and reservation is confirmed and preliminary', () => {
+      const reservation = {
+        id: 'reservation-1',
+        needManualConfirmation: true,
+        state: 'confirmed',
+      };
+      const selected = getSelected({
+        'ui.reservations.toCancel': [reservation],
+      });
+      expect(selected.cancelAllowed).to.be.false;
+    });
   });
 
   describe('isCancellingReservations', () => {
@@ -29,16 +63,23 @@ describe('shared/modals/reservation-cancel/reservationCancelModalSelector', () =
     });
   });
 
-  it('returns reservationsToCancel from the state', () => {
-    const reservationsToCancel = [{ id: 'r-1' }, { id: 'r-2' }];
+  it('returns correct reservation from the state', () => {
+    const reservation = { id: 'reservation-1' };
     const selected = getSelected({
-      'ui.reservations.toCancel': reservationsToCancel,
+      'ui.reservations.toCancel': [reservation],
     });
-    expect(selected.reservationsToCancel).to.deep.equal(reservationsToCancel);
+    expect(selected.reservation).to.deep.equal(reservation);
   });
 
-  it('returns resources from the state', () => {
-    expect(getSelected().resources).to.exist;
+  it('returns correct resource from the state', () => {
+    const resource = { id: 'resource-1' };
+    const reservation = { id: 'reservation-1', resource: resource.id };
+    const selected = getSelected({
+      'data.resources': { [resource.id]: resource },
+      'ui.reservations.toCancel': [reservation],
+    });
+
+    expect(selected.resource).to.deep.equal(resource);
   });
 
   describe('show', () => {
