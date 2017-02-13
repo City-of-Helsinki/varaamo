cad6471e390edd41a29bf537274a4594cadf618f
Lauri Raivio <lauri.raivio@gmail.com>
2017-02-13 13:38:13 +0200

    Improve server side code
    Moves auth related server code to separate module.
    Refs #494.

---
+++ server/auth/getAuthState.js
@@ -0,0 +1,11 @@
+function getAuthState(req) {
+  const user = req.user;
+  if (user && user.token) {
+    return {
+      auth: { userId: user.id, token: user.token },
+    };
+  }
+  return {};
+}
+
+export default getAuthState;
---
+++ server/auth/index.js
@@ -0,0 +1,43 @@
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cookieSession from 'cookie-session';
+import express from 'express';
+
+import configurePassport from './configurePassport';
+import getAuthState from './getAuthState';
+
+const router = express.Router();  // eslint-disable-line new-cap
+const passport = configurePassport();
+const maxSessionAge = 4 * 60 * 60 * 1000;  // 4 hours
+
+// Session handling
+router.use(cookieParser());
+router.use(bodyParser.urlencoded({ extended: true }));
+router.use(cookieSession({
+  secret: process.env.SESSION_SECRET,
+  maxAge: maxSessionAge,
+}));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+router.use(passport.initialize());
+router.use(passport.session());
+
+router.get('/auth', (req, res) => {
+  res.json(getAuthState(req));
+});
+
+router.get('/login', passport.authenticate('helsinki'));
+
+router.get('/login/helsinki/return',
+  passport.authenticate('helsinki', { failureRedirect: '/login' }),
+  (req, res) => res.redirect('/')
+);
+
+router.get('/logout', (req, res) => {
+  req.logOut();
+  const redirectUrl = req.query.next || 'https://varaamo.hel.fi';
+  res.redirect(`https://api.hel.fi/sso/logout/?next=${redirectUrl}`);
+});
+
+export default router;
