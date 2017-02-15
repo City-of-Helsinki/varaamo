14a41d588f1f3460082a2e5655d93d4c1aa110d7
Lauri Raivio <lauri.raivio@gmail.com>
2017-02-15 14:23:42 +0200

    Add redux-thunk
    Refs #494.

--- app/state/middleware/index.js
+++ app/state/middleware/index.js
@@ -1,12 +1,14 @@
 import { applyMiddleware } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
 import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import persistState from './persistState';
 import tracking from './tracking';
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 const storeEnhancers = [
+  applyMiddleware(thunk),
   applyMiddleware(apiMiddleware),
   applyMiddleware(tracking),
   persistState,
