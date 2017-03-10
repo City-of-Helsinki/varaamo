0059f572fcaf837719cc11fb50d3d0a877dffe72
Lauri Raivio <lauri.raivio@gmail.com>
2017-03-10 08:18:14 +0200

    Add ReservationEditForm component
    A component for displaying reservation data and allowing editing
    of certain reservation fields.
    Edit functionality is still not copletely finished.
    Refs #543.

---
+++ app/shared/modals/reservation-info/ReservationEditForm.spec.js
@@ -0,0 +1,154 @@
+import { expect } from 'chai';
+import React from 'react';
+import Form from 'react-bootstrap/lib/Form';
+import Immutable from 'seamless-immutable';
+import { Field } from 'redux-form';
+
+import Reservation from 'utils/fixtures/Reservation';
+import Resource from 'utils/fixtures/Resource';
+import { shallowWithIntl } from 'utils/testUtils';
+import { UnconnectedReservationEditForm as ReservationEditForm } from './ReservationEditForm';
+
+describe('shared/modals/reservation-info/ReservationEditForm', () => {
+  const resource = Resource.build();
+  const reservation = Reservation.build({
+    billingAddressCity: 'New York',
+    billingAddressStreet: 'Billing Street 11',
+    billingAddressZip: '99999',
+    reserverId: '112233-123A',
+    comments: 'Just some comments.',
+    eventDescription: 'Jedi mind tricks',
+    eventSubject: 'Jedi Party',
+    numberOfParticipants: 12,
+    reserverAddressCity: 'Mos Eisley',
+    reserverAddressStreet: 'Cantina street 3B',
+    reserverAddressZip: '11111',
+    reserverEmailAddress: 'luke@sky.com',
+    reserverName: 'Luke Skywalker',
+    reserverPhoneNumber: '1234567',
+    resource: resource.id,
+  });
+  const defaultProps = {
+    isEditing: false,
+    isStaff: false,
+    reservation: Immutable(reservation),
+    resource: Immutable(resource),
+    showComments: false,
+  };
+
+  function getWrapper(extraProps = {}) {
+    return shallowWithIntl(<ReservationEditForm {...defaultProps} {...extraProps} />);
+  }
+
+  describe('render', () => {
+    it('renders a Form component', () => {
+      expect(getWrapper().find(Form)).to.have.length(1);
+    });
+
+    describe('reservation data', () => {
+      function getData(props) {
+        return getWrapper(props).find(Form).html();
+      }
+
+      it('renders billingAddressCity', () => {
+        expect(getData()).to.contain(reservation.billingAddressCity);
+      });
+
+      it('renders billingAddressStreet', () => {
+        expect(getData()).to.contain(reservation.billingAddressStreet);
+      });
+
+      it('renders billingAddressZip', () => {
+        expect(getData()).to.contain(reservation.billingAddressZip);
+      });
+
+      describe('comments', () => {
+        it('are rendered if showComments is true', () => {
+          expect(getData({ showComments: true })).to.contain(reservation.comments);
+        });
+
+        it('are not rendered if showComments is false', () => {
+          expect(getData({ showComments: false })).to.not.contain(reservation.comments);
+        });
+      });
+
+      it('renders eventDescription', () => {
+        expect(getData()).to.contain(reservation.eventDescription);
+      });
+
+      it('renders eventSubject', () => {
+        expect(getData()).to.contain(reservation.eventSubject);
+      });
+
+      it('renders numberOfParticipants', () => {
+        expect(getData()).to.contain(reservation.numberOfParticipants);
+      });
+
+      it('renders reserverAddressCity', () => {
+        expect(getData()).to.contain(reservation.reserverAddressCity);
+      });
+
+      it('renders reserverAddressStreet', () => {
+        expect(getData()).to.contain(reservation.reserverAddressStreet);
+      });
+
+      it('renders reserverAddressZip', () => {
+        expect(getData()).to.contain(reservation.reserverAddressZip);
+      });
+
+      it('renders reserverEmailAddress', () => {
+        expect(getData()).to.contain(reservation.reserverEmailAddress);
+      });
+
+      describe('reserverId', () => {
+        it('is rendered if user has staff rights', () => {
+          expect(getData({ isStaff: true })).to.contain(reservation.reserverId);
+        });
+
+        it('is not rendered if user does not have staff rights', () => {
+          expect(getData({ isStaff: false })).to.not.contain(reservation.reserverId);
+        });
+      });
+
+      it('renders reserverName', () => {
+        expect(getData()).to.contain(reservation.reserverName);
+      });
+
+      it('renders reserverPhoneNumber', () => {
+        expect(getData()).to.contain(reservation.reserverPhoneNumber);
+      });
+    });
+
+    describe('form fields', () => {
+      describe('when editing', () => {
+        function getFormField(name) {
+          return getWrapper({ isEditing: true }).find(Field).filter({ name });
+        }
+
+        it('renders a text field for eventSubject', () => {
+          const field = getFormField('eventSubject');
+          expect(field).to.have.length(1);
+          expect(field.prop('type')).to.equal('text');
+        });
+
+        it('renders a textarea field for eventDescription', () => {
+          const field = getFormField('eventDescription');
+          expect(field).to.have.length(1);
+          expect(field.prop('type')).to.equal('textarea');
+        });
+
+        it('renders a number field for numberOfParticipants', () => {
+          const field = getFormField('numberOfParticipants');
+          expect(field).to.have.length(1);
+          expect(field.prop('type')).to.equal('number');
+        });
+      });
+
+      describe('when not editing', () => {
+        it('does not render any form fields', () => {
+          expect(getWrapper({ isEditing: false }).find(Field)).to.have.length(0);
+        });
+      });
+    });
+  });
+});
