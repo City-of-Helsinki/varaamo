2d56934fb1f79d247cc4a56bf4c6fb6f6b194ce6
Raúl García <serulo@gmail.com>
2017-03-21 17:28:55 +0100

    Add AdminResourceTypeFilter actions and reducers
    Refs #548.

--- app/state/reducers/ui/adminResourcesPageReducer.js
+++ app/state/reducers/ui/adminResourcesPageReducer.js
@@ -1,4 +1,5 @@
 import map from 'lodash/map';
+import uniq from 'lodash/uniq';
 import filter from 'lodash/filter';
 import Immutable from 'seamless-immutable';
 
@@ -6,6 +7,7 @@ import types from 'constants/ActionTypes';
 
 const initialState = Immutable({
   date: undefined,
+  filteredResourceTypes: [],
   resourceIds: [],
 });
 
@@ -13,6 +15,19 @@ function adminResourcesPageReducer(state = initialState, action) {
   switch (action.type) {
     case types.UI.CHANGE_ADMIN_RESOURCES_PAGE_DATE: {
       return state.merge({ date: action.payload || undefined });
+    }
+
+    case types.UI.FILTER_ADMIN_RESOURCE_TYPE: {
+      return state.merge({
+        filteredResourceTypes: uniq([...state.filteredResourceTypes, action.payload]),
+      });
+    }
+
+    case types.UI.UNFILTER_ADMIN_RESOURCE_TYPE: {
+      return state.merge({ filteredResourceTypes: filter(
+          state.filteredResourceTypes,
+          resourceType => resourceType !== action.payload
+        ) });
     }
 
     case types.API.RESOURCES_GET_SUCCESS: {
