cbc892a2c1c4dcffae05ac1b58ec62c6a2c3205c
Raúl García <serulo@gmail.com>
2017-03-23 09:12:22 +0100

    Adds ResourceTypeFilter to AdminResourcesPage
    Refs #548.

--- app/pages/admin-resources/AdminResourcesPage.js
+++ app/pages/admin-resources/AdminResourcesPage.js
@@ -5,9 +5,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { fetchFavoritedResources } from 'actions/resourceActions';
-import { changeAdminResourcesPageDate, openConfirmReservationModal } from 'actions/uiActions';
+import {
+  changeAdminResourcesPageDate,
+  filterAdminResourceType,
+  openConfirmReservationModal,
+  unfilterAdminResourceType,
+} from 'actions/uiActions';
 import PageWrapper from 'pages/PageWrapper';
 import AvailabilityView from 'shared/availability-view';
+import ResourceTypeFilter from 'shared/resource-type-filter';
 import ReservationSuccessModal from 'shared/modals/reservation-success';
 import ReservationConfirmationContainer from 'shared/reservation-confirmation';
 import { injectT } from 'i18n';
@@ -49,22 +55,32 @@ class UnconnectedAdminResourcesPage extends Component {
 
   render() {
     const {
+      filteredResourceTypes,
       isAdmin,
       isFetchingResources,
       resources,
       t,
+      resourceTypes,
     } = this.props;
     return (
       <PageWrapper className="admin-resources-page" title={t('AdminResourcesPage.title')}>
         <h1>{t('AdminResourcesPage.title')}</h1>
         <Loader loaded={Boolean(!isFetchingResources || resources.length)}>
           {isAdmin && (
-            <AvailabilityView
-              date={this.props.date}
-              groups={[{ name: '', resources }]}
-              onDateChange={this.props.actions.changeAdminResourcesPageDate}
-              onSelect={this.handleSelect}
-            />
+            <div>
+              <ResourceTypeFilter
+                filteredResourceTypes={filteredResourceTypes}
+                onFilterResourceType={this.props.actions.filterAdminResourceType}
+                onUnfilterResourceType={this.props.actions.unfilterAdminResourceType}
+                resourceTypes={resourceTypes}
+              />
+              <AvailabilityView
+                date={this.props.date}
+                groups={[{ name: '', resources }]}
+                onDateChange={this.props.actions.changeAdminResourcesPageDate}
+                onSelect={this.handleSelect}
+              />
+            </div>
           )}
           {isAdmin && !resources.length && <p>{t('AdminResourcesPage.noResourcesMessage')}</p>}
           {!isAdmin && (
@@ -89,10 +105,12 @@ class UnconnectedAdminResourcesPage extends Component {
 UnconnectedAdminResourcesPage.propTypes = {
   actions: PropTypes.object.isRequired,
   date: PropTypes.string.isRequired,
+  filteredResourceTypes: PropTypes.arrayOf(PropTypes.string).isRequired,
   isAdmin: PropTypes.bool.isRequired,
   isFetchingResources: PropTypes.bool.isRequired,
   resources: PropTypes.array.isRequired,
   t: PropTypes.func.isRequired,
+  resourceTypes: PropTypes.array.isRequired,
 };
 
 UnconnectedAdminResourcesPage = injectT(UnconnectedAdminResourcesPage);  // eslint-disable-line
@@ -101,7 +119,9 @@ function mapDispatchToProps(dispatch) {
   const actionCreators = {
     changeAdminResourcesPageDate,
     fetchFavoritedResources,
+    filterAdminResourceType,
     openConfirmReservationModal,
+    unfilterAdminResourceType,
   };
 
   return { actions: bindActionCreators(actionCreators, dispatch) };
