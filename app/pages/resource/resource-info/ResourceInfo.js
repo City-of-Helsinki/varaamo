0a71d4eb79b55044433da155c407724609751494
Raúl García <serulo@gmail.com>
2017-03-27 17:48:49 +0200

    Style ResourceInfo to be aside resource images
    Refs #548

--- app/pages/resource/resource-info/ResourceInfo.js
+++ app/pages/resource/resource-info/ResourceInfo.js
@@ -1,6 +1,10 @@
 import upperFirst from 'lodash/upperFirst';
 import React, { PropTypes } from 'react';
+import Col from 'react-bootstrap/lib/Col';
+import Label from 'react-bootstrap/lib/Label';
+import Row from 'react-bootstrap/lib/Row';
 
+import { injectT } from 'i18n';
 import WrappedText from 'shared/wrapped-text';
 import FavoriteButton from 'shared/favorite-button';
 import ResourceIcons from 'shared/resource-icons';
@@ -15,23 +19,50 @@ function getAddress({ addressZip, municipality, name, streetAddress }) {
   return parts.filter(part => part).join(', ');
 }
 
-function ResourceInfo({ isAdmin, resource, unit }) {
+function renderEquipment(equipment, t) {
+  return equipment.length ?
+    <div className="details-row resource-equipment">
+      <div className="details-label">{t('ResourceInfo.equipmentHeader')}</div>
+      {
+        equipment.map(item =>
+          <Label bsStyle="primary" key={`label-${item.id}`}>{item.name.fi}</Label>
+        )
+      }
+    </div> :
+    null;
+}
+
+function ResourceInfo({ isAdmin, resource, unit, t }) {
   return (
-    <div className="resource-info">
-      {isAdmin && <FavoriteButton resource={resource} />}
-      <h1>{resource.name}</h1>
-      <p className="address lead">{getAddress(unit)}</p>
-      <ResourceIcons resource={resource} />
-      <ImageCarousel images={resource.images || []} />
-      <WrappedText text={resource.description} />
-    </div>
+    <section className="resource-info">
+      <Row>
+        <Col className="resource-images" sm={7} xs={12}>
+          <ImageCarousel images={resource.images || []} />
+        </Col>
+        <Col sm={5} xs={12}>
+          {isAdmin && <FavoriteButton resource={resource} />}
+          <h1>{resource.name}</h1>
+          <p className="address lead">{getAddress(unit)}</p>
+          <ResourceIcons resource={resource} />
+          {renderEquipment(resource.equipment, t)}
+          {resource.description &&
+            <div className="resource-description">
+              <WrappedText text={resource.description} />
+            </div>
+          }
+        </Col>
+      </Row>
+    </section>
   );
 }
 
 ResourceInfo.propTypes = {
   isAdmin: PropTypes.bool.isRequired,
   resource: PropTypes.object.isRequired,
+  t: PropTypes.func.isRequired,
   unit: PropTypes.object.isRequired,
 };
+
+ResourceInfo = injectT(ResourceInfo);  // eslint-disable-line
 
 export default ResourceInfo;
--- app/pages/resource/resource-info/ResourceInfo.spec.js
+++ app/pages/resource/resource-info/ResourceInfo.spec.js
@@ -1,11 +1,12 @@
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
 import React from 'react';
+import Label from 'react-bootstrap/lib/Label';
 import Immutable from 'seamless-immutable';
 
 import WrappedText from 'shared/wrapped-text';
 import Resource from 'utils/fixtures/Resource';
 import Unit from 'utils/fixtures/Unit';
+import { shallowWithIntl } from 'utils/testUtils';
 import FavoriteButton from 'shared/favorite-button';
 import ResourceIcons from 'shared/resource-icons';
 import ImageCarousel from './ImageCarousel';
@@ -22,7 +23,7 @@ describe('pages/resource/resource-info/ResourceInfo', () => {
   };
 
   function getWrapper(extraProps) {
-    return shallow(<ResourceInfo {...defaultProps} {...extraProps} />);
+    return shallowWithIntl(<ResourceInfo {...defaultProps} {...extraProps} />);
   }
 
   describe('FavoriteButton', () => {
@@ -71,6 +72,30 @@ describe('pages/resource/resource-info/ResourceInfo', () => {
 
     expect(imageCarousel.length).to.equal(1);
     expect(imageCarousel.prop('images')).to.deep.equal(defaultProps.resource.images);
+  });
+
+  it('renders resource equipment', () => {
+    const equippedResource = {
+      ...defaultProps.resource,
+      equipment: [
+        { id: 1, name: { fi: 'projector' } },
+        { id: 2, name: { fi: 'whiteboard' } },
+      ],
+    };
+    const resourceEquipment = (
+      getWrapper({ resource: equippedResource }).find('.resource-equipment')
+    );
+    const nameLabel = resourceEquipment.find('.details-label');
+    expect(nameLabel.text()).to.equal('ResourceInfo.equipmentHeader');
+    const equipmentLabels = resourceEquipment.find(Label);
+    expect(equipmentLabels).to.have.length(2);
+    expect(equipmentLabels.children().get(0)).to.equal('projector');
+    expect(equipmentLabels.children().get(1)).to.equal('whiteboard');
+  });
+
+  it('does not render resource equipment if empty', () => {
+    const resourceEquipment = getWrapper().find('.resource-equipment');
+    expect(resourceEquipment).to.have.length(0);
   });
 
   it('renders resource description as WrappedText', () => {
