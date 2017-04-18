29d283f65e0cb67cb2b88ac5e7a6fb2c26cd6d56
Lauri Raivio <lauri.raivio@gmail.com>
2017-04-18 20:11:24 +0300

    Add fail reason to failed reservations
    Refs #572.

--- app/shared/modals/reservation-success/ReservationSuccessModal.js
+++ app/shared/modals/reservation-success/ReservationSuccessModal.js
@@ -48,6 +48,7 @@ function ReservationSuccessModal({
           <CompactReservationList
             className="failed-reservations-list"
             reservations={failedReservations}
+            subtitle="failReason"
           />
         </div>
       }
