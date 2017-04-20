d1f85957aef8c6d622db117358da92f6df81f223
Raúl García <serulo@gmail.com>
2017-04-20 15:23:03 +0200

    Add datepicker to recurring reservation controls
    Adds datepicker and give styles to recurring reservation controls.
    Refs #572.

--- app/shared/recurring-reservation-controls/RecurringReservationControls.js
+++ app/shared/recurring-reservation-controls/RecurringReservationControls.js
@@ -1,10 +1,13 @@
 import React, { PropTypes } from 'react';
+import Col from 'react-bootstrap/lib/Col';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import FormControl from 'react-bootstrap/lib/FormControl';
 import FormGroup from 'react-bootstrap/lib/FormGroup';
+import Row from 'react-bootstrap/lib/Row';
 import Select from 'react-select';
 
 import { injectT } from 'i18n';
+import DatePicker from 'shared/date-picker';
 
 function createOptionRenderer(t) {
   return option => t(option.label);
@@ -14,50 +17,85 @@ function createValueRenderer(t) {
   return option => t(option.label);
 }
 
-function RecurringReservationControls(props) {
-  if (!props.isVisible) {
+function RecurringReservationControls({
+  changeFrequency,
+  changeLastTime,
+  changeNumberOfOccurrences,
+  frequency,
+  frequencyOptions,
+  isVisible,
+  numberOfOccurrences,
+  lastTime,
+  t,
+}) {
+  if (!isVisible) {
     return <span />;
   }
   return (
     <div className="recurring-reservation-controls">
-      <div className="recurring-reservation-frequency-control">
-        <label htmlFor="recurrence-frequency-select">
-          {props.t('RecurringReservationControls.frequencyLabel')}
-        </label>
-        <Select
-          className="recurrence-frequency-select"
-          clearable={false}
-          inputProps={{ id: 'recurrence-frequency-select' }}
-          name="recurrence-frequency-select"
-          onChange={props.changeFrequency}
-          optionRenderer={createOptionRenderer(props.t)}
-          options={props.frequencyOptions}
-          value={props.frequency}
-          valueRenderer={createValueRenderer(props.t)}
-        />
-      </div>
-      <FormGroup controlId="numberOfOccurrencesGroup">
-        <ControlLabel>
-          {props.t('RecurringReservationControls.numberOfOccurrencesLabel')}
-        </ControlLabel>
-        <FormControl
-          min={1}
-          onChange={event => props.changeNumberOfOccurrences(event.target.value || 1)}
-          type="number"
-          value={props.numberOfOccurrences}
-        />
-      </FormGroup>
+      <Row>
+        <Col sm={5} xs={12}>
+          <div className="recurring-reservation-frequency-control">
+            <label htmlFor="recurrence-frequency-select">
+              {t('RecurringReservationControls.frequencyLabel')}
+            </label>
+            <Select
+              className="recurrence-frequency-select"
+              clearable={false}
+              inputProps={{ id: 'recurrence-frequency-select' }}
+              name="recurrence-frequency-select"
+              onChange={changeFrequency}
+              optionRenderer={createOptionRenderer(t)}
+              options={frequencyOptions}
+              value={frequency}
+              valueRenderer={createValueRenderer(t)}
+            />
+          </div>
+        </Col>
+        {frequency !== '' &&
+          <Col sm={3} xs={12}>
+            <FormGroup controlId="numberOfOccurrencesGroup">
+              <ControlLabel>
+                {t('RecurringReservationControls.numberOfOccurrencesLabel')}
+              </ControlLabel>
+              <FormControl
+                min={1}
+                onChange={event => changeNumberOfOccurrences(event.target.value || 1)}
+                type="number"
+                value={numberOfOccurrences}
+              />
+            </FormGroup>
+          </Col>
+        }
+        {frequency !== '' &&
+          <Col sm={4} xs={12}>
+            <FormGroup controlId="LastTimeGroup">
+              <ControlLabel>
+                {t('RecurringReservationControls.lastTimeLabel')}
+              </ControlLabel>
+              <DatePicker
+                dateFormat="dd D.M.YYYY"
+                formControl
+                onChange={changeLastTime}
+                value={lastTime}
+              />
+            </FormGroup>
+          </Col>
+        }
+      </Row>
     </div>
   );
 }
 
 RecurringReservationControls.propTypes = {
   changeFrequency: PropTypes.func.isRequired,
+  changeLastTime: PropTypes.func.isRequired,
   changeNumberOfOccurrences: PropTypes.func.isRequired,
   frequency: PropTypes.string.isRequired,
   frequencyOptions: PropTypes.array.isRequired,
   isVisible: PropTypes.bool.isRequired,
   numberOfOccurrences: PropTypes.number.isRequired,
+  lastTime: PropTypes.string,
   t: PropTypes.func.isRequired,
 };
 
--- app/shared/recurring-reservation-controls/RecurringReservationControls.spec.js
+++ app/shared/recurring-reservation-controls/RecurringReservationControls.spec.js
@@ -2,7 +2,9 @@ import { expect } from 'chai';
 import React from 'react';
 import FormControl from 'react-bootstrap/lib/FormControl';
 import Select from 'react-select';
+import simple from 'simple-mock';
 
+import DatePicker from 'shared/date-picker';
 import { shallowWithIntl } from 'utils/testUtils';
 import RecurringReservationControls from './RecurringReservationControls';
 
@@ -10,9 +12,11 @@ function getWrapper(props) {
   const defaults = {
     changeFrequency: () => null,
     changeNumberOfOccurrences: () => null,
+    changeLastTime: () => null,
     frequency: 'days',
     frequencyOptions: [{ label: '', value: '' }],
     isVisible: true,
+    lastTime: null,
     numberOfOccurrences: 1,
   };
   return shallowWithIntl(<RecurringReservationControls {...defaults} {...props} />);
@@ -29,6 +33,7 @@ describe('shared/RecurringReservationControls/RecurringReservationControls', () 
       changeFrequency: () => null,
       frequency: 'days',
       frequencyOptions: [{ label: '', value: '' }],
+      lastTime: '2017-04-09',
     };
     const select = getWrapper(props).find(Select);
     expect(select).to.have.length(1);
@@ -39,11 +44,41 @@ describe('shared/RecurringReservationControls/RecurringReservationControls', () 
 
   it('renders FormControl to change number of occurrences', () => {
     const props = {
+      lastTime: '2017-04-09',
       numberOfOccurrences: 12,
     };
     const control = getWrapper(props).find(FormControl);
     expect(control).to.have.length(1);
     expect(control.prop('min')).to.equal(1);
     expect(control.prop('value')).to.equal(props.numberOfOccurrences);
+  });
+
+  it('renders DatePicker with correct props', () => {
+    const props = {
+      lastTime: '2017-04-09',
+      numberOfOccurrences: 12,
+      changeLastTime: simple.mock(),
+    };
+    const control = getWrapper(props).find(DatePicker);
+    expect(control).to.have.length(1);
+    expect(control.prop('dateFormat')).to.equal('dd D.M.YYYY');
+    expect(control.prop('formControl')).to.be.true;
+    expect(control.prop('onChange')).to.equal(props.changeLastTime);
+    expect(control.prop('value')).to.equal(props.lastTime);
+  });
+
+  describe('without set frecuency', () => {
+    let wrapper;
+    before(() => {
+      wrapper = getWrapper({ frequency: '' });
+    });
+
+    it('does not render numbrerOfOcurrences FormControl', () => {
+      expect(wrapper.find(FormControl)).to.have.length(0);
+    });
+
+    it('does not render DatePicker', () => {
+      expect(wrapper.find(DatePicker)).to.have.length(0);
+    });
   });
 });
