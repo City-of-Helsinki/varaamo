3129ab19b353e6d800911483088049889a1d8579
Lauri Raivio <lauri.raivio@gmail.com>
2017-05-26 08:59:09 +0300

    Add Font Awesome for more awesome icons
    Closes #634.

--- server/Html.js
+++ server/Html.js
@@ -64,6 +64,7 @@ class Html extends Component {
           <meta charSet="utf-8" />
           <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
           <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
           <link href="https://overpass-30e2.kxcdn.com/overpass.css" rel="stylesheet" />
           {this.renderStylesLink(appCssSrc, isProduction)}
           <title>Varaamo</title>
