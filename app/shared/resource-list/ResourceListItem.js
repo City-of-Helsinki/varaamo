b0dce46514d44f3fd2063279ef12adf2c6891ea7
Raúl García <serulo@gmail.com>
2017-05-29 18:57:47 +0200

    Update styles for ResourceListItem content
    Refs #630.

--- app/shared/resource-list/ResourceListItem.js
+++ app/shared/resource-list/ResourceListItem.js
@@ -2,15 +2,21 @@ import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 
 import BackgroundImage from 'shared/background-image';
-import ResourceIcons from 'shared/resource-icons';
 import { getMainImage } from 'utils/imageUtils';
 import { getResourcePageUrl } from 'utils/resourceUtils';
-import ReserveButton from './ReserveButton';
+import Label from 'shared/label';
 import ResourceAvailability from './ResourceAvailability';
 
 class ResourceListItem extends Component {
+  renderEquipment(equipment) {
+    return (
+      <Label key={equipment.id} shape="rounded" size="mini" theme="gold">
+        {equipment.name}
+      </Label>
+    );
+  }
   render() {
-    const { isLoggedIn, resource, unit } = this.props;
+    const { resource, unit } = this.props;
     const date = this.context.location.query.date;
 
     return (
@@ -20,19 +26,20 @@ class ResourceListItem extends Component {
             height={420}
             image={getMainImage(resource.images)}
             width={700}
-          >
-            <ResourceAvailability date={date} resource={resource} />
-          </BackgroundImage>
+          />
         </Link>
-          <ResourceIcons resource={resource} />
         <div className="app-ResourceListItem__content">
           <Link to={getResourcePageUrl(resource, date)}>
             <h4>{resource.name}</h4>
           </Link>
-          <div className="unit-name">{unit.name}</div>
-          <div className="controls">
-            <ReserveButton date={date} isLoggedIn={isLoggedIn} resource={resource} />
+          <div className="app-ResourceListItem__unit-name">
+            <span>{unit.name}</span>
+            {resource.type && <Label size="mini" theme="blue">{resource.type.name}</Label>}
           </div>
+          <div className="app-ResourceListItem__equipment">
+            {resource.equipment.map(this.renderEquipment)}
+          </div>
+          <ResourceAvailability date={date} resource={resource} />
         </div>
       </li>
     );
@@ -40,7 +47,6 @@ class ResourceListItem extends Component {
 }
 
 ResourceListItem.propTypes = {
-  isLoggedIn: PropTypes.bool.isRequired,
   resource: PropTypes.object.isRequired,
   unit: PropTypes.object.isRequired,
 };
