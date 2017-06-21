821f6bd5baf924893827e07058946d4287ffbba4
Raúl García <serulo@gmail.com>
2017-06-21 11:37:42 +0200

    Render ResourceCards in ResourceCompactList
    Refs #660

--- app/shared/resource-compact-list/ResourceCompactList.js
+++ app/shared/resource-compact-list/ResourceCompactList.js
@@ -1,23 +1,65 @@
 import React, { PropTypes } from 'react';
+import Glyphicon from 'react-bootstrap/lib/Glyphicon';
 import { connect } from 'react-redux';
 
 import ResourceCard from 'shared/resource-card';
 import selector from './ResourceCompactListSelector';
 
-function renderResourceListItem(resourceId) {
-  return <ResourceCard key={resourceId} resourceId={resourceId} />;
+export class UnconnectedResourceCompactList extends React.Component {
+  static propTypes = {
+    resourceIds: PropTypes.array.isRequired,
+  };
+
+  state = {
+    resourcePosition: 0,
+  }
+
+  componentWillReceiveProps() {
+    this.setState({
+      resourcePosition: 0,
+    });
+  }
+
+  onPreviousResource = () => {
+    this.setState({
+      resourcePosition: (this.state.resourcePosition - 1) % this.props.resourceIds.length,
+    });
+  }
+
+  onNextResource = () => {
+    this.setState({
+      resourcePosition: (this.state.resourcePosition + 1) % this.props.resourceIds.length,
+    });
+  }
+
+  render() {
+    return (
+      <div className="app-ResourceCompactList">
+        {this.state.resourcePosition !== 0 &&
+          <button
+            className="app-ResourceCompactList_arrow app-ResourceCompactList_arrow-left"
+            onClick={this.onPreviousResource}
+          >
+            <Glyphicon glyph="chevron-left" />
+          </button>
+        }
+        <ResourceCard
+          resourceId={this.props.resourceIds[this.state.resourcePosition]}
+        />;
+        {
+          this.state.resourcePosition !== this.props.resourceIds.length - 1 &&
+          this.props.resourceIds.length &&
+          <button
+            className="app-ResourceCompactList_arrow app-ResourceCompactList_arrow-right"
+            onClick={this.onNextResource}
+          >
+            <Glyphicon glyph="chevron-right" />
+          </button>
+        }
+      </div>
+    );
+  }
 }
 
-export function UnconnectedResourceCompactList({ resourceIds }) {
-  return (
-    <div className="resource-compact-list">
-      {resourceIds.map(renderResourceListItem)}
-    </div>
-  );
-}
-
-UnconnectedResourceCompactList.propTypes = {
-  resourceIds: PropTypes.array.isRequired,
-};
 
 export default connect(selector)(UnconnectedResourceCompactList);
---
+++ app/shared/resource-compact-list/ResourceCompactList.spec.js
@@ -0,0 +1,129 @@
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+import Immutable from 'seamless-immutable';
+
+import ResourceCard from 'shared/resource-card';
+import { UnconnectedResourceCompactList as ResourceCompactList } from './ResourceCompactList';
+
+describe('shared/resource-list/ResourceCompactList', () => {
+  const defaultProps = {
+    resourceIds: Immutable(['resource-1', 'resource-2']),
+  };
+
+  function getWrapper(extraProps) {
+    return shallow(<ResourceCompactList {...defaultProps} {...extraProps} />);
+  }
+
+  describe('with resourceIds', () => {
+    let wrapper;
+
+    beforeEach(() => {
+      wrapper = getWrapper();
+    });
+
+    it('renders a div with correct className', () => {
+      const div = wrapper.find('div.app-ResourceCompactList');
+      expect(div.length).to.equal(1);
+    });
+
+    it('renders first ResourceCard', () => {
+      const resourceCard = wrapper.find(ResourceCard);
+      expect(resourceCard.length).to.equal(1);
+      expect(resourceCard.prop('resourceId')).to.equal('resource-1');
+    });
+
+    it('renders second ResourceCard if resourcePosition is 1', () => {
+      const instance = wrapper.instance();
+      instance.setState({
+        resourcePosition: 1,
+      });
+      wrapper.update();
+      const resourceCard = wrapper.find(ResourceCard);
+      expect(resourceCard.length).to.equal(1);
+      expect(resourceCard.prop('resourceId')).to.equal('resource-2');
+    });
+
+    it('renders left arrow if resourcePosition state is not 0', () => {
+      const instance = wrapper.instance();
+      instance.setState({
+        resourcePosition: 1,
+      });
+      wrapper.update();
+      const leftArrow = wrapper.find('.app-ResourceCompactList_arrow-left');
+      expect(leftArrow.length).to.equal(1);
+    });
+
+    it('does not render left arrow if resourcePosition state is 0', () => {
+      const instance = wrapper.instance();
+      instance.setState({
+        resourcePosition: 0,
+      });
+      wrapper.update();
+      const leftArrow = wrapper.find('.app-ResourceCompactList_arrow-left');
+      expect(leftArrow.length).to.equal(0);
+    });
+
+    it('left arrow has correct onClick prop', () => {
+      const instance = wrapper.instance();
+      instance.setState({
+        resourcePosition: 1,
+      });
+      wrapper.update();
+      const leftArrow = wrapper.find('.app-ResourceCompactList_arrow-left');
+      expect(leftArrow.prop('onClick')).to.equal(instance.onPreviousResource);
+    });
+
+    it('renders right arrow if resourcePosition state is 0', () => {
+      const instance = wrapper.instance();
+      instance.setState({
+        resourcePosition: 0,
+      });
+      wrapper.update();
+      const rightArrow = wrapper.find('.app-ResourceCompactList_arrow-right');
+      expect(rightArrow.length).to.equal(1);
+    });
+
+    it('does not render right arrow if resourcePosition state is last resource position', () => {
+      const instance = wrapper.instance();
+      instance.setState({
+        resourcePosition: 1,
+      });
+      wrapper.update();
+      const rightArrow = wrapper.find('.app-ResourceCompactList_arrow-right');
+      expect(rightArrow.length).to.equal(0);
+    });
+
+    it('right arrow has correct onClick prop', () => {
+      const instance = wrapper.instance();
+      instance.setState({
+        resourcePosition: 0,
+      });
+      wrapper.update();
+      const rightArrow = wrapper.find('.app-ResourceCompactList_arrow-right');
+      expect(rightArrow.prop('onClick')).to.equal(instance.onNextResource);
+    });
+  });
+
+  describe('onPreviousResource', () => {
+    it('decreases resourcePosition by 1', () => {
+      const instance = getWrapper().instance();
+      instance.setState({
+        resourcePosition: 1,
+      });
+      instance.onPreviousResource();
+      expect(instance.state.resourcePosition).to.equal(0);
+    });
+  });
+
+  describe('onNextResource', () => {
+    it('decreases resourcePosition by 1', () => {
+      const instance = getWrapper().instance();
+      instance.setState({
+        resourcePosition: 0,
+      });
+      instance.onNextResource();
+      expect(instance.state.resourcePosition).to.equal(1);
+    });
+  });
+});
