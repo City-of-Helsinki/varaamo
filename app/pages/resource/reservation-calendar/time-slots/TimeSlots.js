9b1882f71d0a2a84dd130a917b8a936a36c46692
Lauri Raivio <lauri.raivio@gmail.com>
2017-06-26 03:30:25 +0300

    Update resource page time slots styles
    Refs #661.

--- app/pages/resource/reservation-calendar/time-slots/TimeSlots.js
+++ app/pages/resource/reservation-calendar/time-slots/TimeSlots.js
@@ -1,24 +1,29 @@
 import includes from 'lodash/includes';
 import React, { Component, PropTypes } from 'react';
-import Table from 'react-bootstrap/lib/Table';
 import Loader from 'react-loader';
 
-import { injectT } from 'i18n';
 import TimeSlot from './TimeSlot';
 
 class TimeSlots extends Component {
-  constructor(props) {
-    super(props);
-    this.renderTimeSlot = this.renderTimeSlot.bind(this);
-  }
+  static propTypes = {
+    addNotification: PropTypes.func.isRequired,
+    isAdmin: PropTypes.bool.isRequired,
+    isEditing: PropTypes.bool.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    isLoggedIn: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired,
+    resource: PropTypes.object.isRequired,
+    selected: PropTypes.array.isRequired,
+    slots: PropTypes.array.isRequired,
+    time: PropTypes.string,
+  };
 
-  renderTimeSlot(slot) {
+  renderTimeSlot = (slot) => {
     const {
       addNotification,
       isAdmin,
       isEditing,
       isLoggedIn,
-      isStaff,
       onClick,
       resource,
       selected,
@@ -32,7 +37,6 @@ class TimeSlots extends Component {
         isAdmin={isAdmin}
         isEditing={isEditing}
         isLoggedIn={isLoggedIn}
-        isStaff={isStaff}
         key={slot.start}
         onClick={onClick}
         resource={resource}
@@ -44,53 +48,16 @@ class TimeSlots extends Component {
   }
 
   render() {
-    const {
-      isAdmin,
-      isFetching,
-      slots,
-      t,
-    } = this.props;
+    const { isFetching, slots } = this.props;
 
     return (
       <Loader loaded={!isFetching}>
-        <Table
-          className="time-slots lined"
-          hover
-          responsive
-        >
-          <thead>
-            <tr>
-              <th />
-              <th>{t('TimeSlots.time')}</th>
-              <th>{t('TimeSlots.reservations')}</th>
-              {!isAdmin && <th />}
-              {isAdmin && <th>{t('TimeSlots.reserver')}</th>}
-              {isAdmin && <th>{t('TimeSlots.comments')}</th>}
-              {isAdmin && <th>{t('TimeSlots.controls')}</th>}
-            </tr>
-          </thead>
-          <tbody>
-            {slots.map(this.renderTimeSlot)}
-          </tbody>
-        </Table>
+        <div className="app-TimeSlots">
+          {slots.map(this.renderTimeSlot)}
+        </div>
       </Loader>
     );
   }
 }
 
-TimeSlots.propTypes = {
-  addNotification: PropTypes.func.isRequired,
-  isAdmin: PropTypes.bool.isRequired,
-  isEditing: PropTypes.bool.isRequired,
-  isFetching: PropTypes.bool.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired,
-  isStaff: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired,
-  resource: PropTypes.object.isRequired,
-  selected: PropTypes.array.isRequired,
-  slots: PropTypes.array.isRequired,
-  t: PropTypes.func.isRequired,
-  time: PropTypes.string,
-};
-
-export default injectT(TimeSlots);
+export default TimeSlots;
