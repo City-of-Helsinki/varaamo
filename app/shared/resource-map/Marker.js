e5ebf1d261810c40804d9b10aee5f41aacf5b8aa
Raúl García <serulo@gmail.com>
2017-06-27 17:06:34 +0200

    Add number of resources to marker's icon
    Refs #666.

--- app/shared/resource-map/Marker.js
+++ app/shared/resource-map/Marker.js
@@ -1,12 +1,9 @@
 import L from 'leaflet';
 import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
-import iconUrl from 'leaflet/dist/images/marker-icon.png';
-import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
 import React, { PropTypes } from 'react';
-import { Marker as LeafletMarker, Tooltip } from 'react-leaflet';
+import { Marker as LeafletMarker } from 'react-leaflet';
 
 import highlightedIconRetinaUrl from 'assets/map/highlighted-marker-icon-2x.png';
-import highlightedIconUrl from 'assets/map/highlighted-marker-icon.png';
 
 class Marker extends React.Component {
   handleClick = () => {
@@ -14,39 +11,31 @@ class Marker extends React.Component {
   }
 
   render() {
-    // Override default Leaflet icon as there is a bug with leaflet and webpack
-    // ref: https://github.com/PaulLeCam/react-leaflet/issues/255
-    const icon = L.icon({
-      iconAnchor: [12, 41],
-      iconRetinaUrl,
-      iconSize: [25, 41],
-      iconUrl,
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41],
-      shadowUrl,
-      tooltipAnchor: [16, -28],
+    const commonIcon = {
+      className: 'marker-icon',
+      iconAnchor: [12, 48],
+      iconSize: [25, 48],
+    };
+    const count = (
+      this.props.resourceIds.length > 1 ?
+      this.props.resourceIds.length :
+      ''
+    );
+    const icon = L.divIcon({
+      ...commonIcon,
+      html: `<div>${count}</div><img src="${iconRetinaUrl}" />`,
     });
 
-    const highlightedIcon = L.icon({
-      iconAnchor: [12, 41],
-      iconRetinaUrl: highlightedIconRetinaUrl,
-      iconSize: [25, 41],
-      iconUrl: highlightedIconUrl,
-      popupAnchor: [1, -34],
-      shadowSize: [41, 41],
-      shadowUrl,
-      tooltipAnchor: [16, -28],
+    const highlightedIcon = L.divIcon({
+      ...commonIcon,
+      html: `<div>${count}</div><img src="${highlightedIconRetinaUrl}" />`,
     });
     return (
       <LeafletMarker
         icon={this.props.highlighted ? highlightedIcon : icon}
         onClick={this.handleClick}
         position={[this.props.latitude, this.props.longitude]}
-      >
-        {this.props.resourceIds.length > 1 &&
-          <Tooltip direction="top"><span>{this.props.resourceIds.length}</span></Tooltip>
-        }
-      </LeafletMarker>
+      />
     );
   }
 }
