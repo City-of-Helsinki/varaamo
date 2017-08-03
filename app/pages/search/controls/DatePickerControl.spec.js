914bd8cfff8d425f461d94186fe784dc5cbffbf7
Lauri Raivio <lauri.raivio@gmail.com>
2017-08-03 09:16:50 +0300

    Update DatePickerControl to not use MiniModal
    Refs #726.

--- app/pages/search/controls/DatePickerControl.spec.js
+++ app/pages/search/controls/DatePickerControl.spec.js
@@ -1,9 +1,9 @@
 import { expect } from 'chai';
 import React from 'react';
+import Modal from 'react-bootstrap/lib/Modal';
 import { Calendar } from 'react-date-picker';
 import simple from 'simple-mock';
 
-import MiniModal from 'shared/mini-modal';
 import { shallowWithIntl } from 'utils/testUtils';
 import DatePickerControl from './DatePickerControl';
 
@@ -21,10 +21,12 @@ describe('pages/search/controls/DatePickerControl', () => {
     expect(wrapper.is('div.app-DatePickerControl')).to.be.true;
   });
 
-  it('renders MiniModal with correct theme', () => {
-    const miniModal = getWrapper().find(MiniModal);
-    expect(miniModal).to.have.length(1);
-    expect(miniModal.prop('theme')).to.equal('green');
+  it('renders Modal with correct props', () => {
+    const wrapper = getWrapper();
+    const modal = wrapper.find(Modal);
+    expect(modal).to.have.length(1);
+    expect(modal.prop('onHide')).to.equal(wrapper.instance().hideModal);
+    expect(modal.prop('show')).to.equal(wrapper.instance().state.visible);
   });
 
   it('renders calendar for selecting date', () => {
@@ -33,36 +35,43 @@ describe('pages/search/controls/DatePickerControl', () => {
     expect(calendar).to.have.length(1);
     expect(calendar.prop('dateFormat')).to.equal('L');
     expect(calendar.prop('defaultDate')).to.equal(defaults.value);
-    expect(calendar.prop('onChange')).to.equal(wrapper.instance().handleChange);
-  });
-
-  describe('componentWillReceiveProps', () => {
-    it('sets state.value to the nextProps.value', () => {
-      const value = '28.06.2017';
-      const instance = getWrapper({ value }).instance();
-      instance.componentWillReceiveProps({ value: '01.01.2017' });
-      expect(instance.state.value).to.equal('01.01.2017');
-    });
-  });
-
-  describe('handleChange', () => {
-    it('sets state.value to the new value', () => {
-      const instance = getWrapper().instance();
-      instance.state.value = '28.06.2017';
-      instance.handleChange('01.01.2017');
-      expect(instance.state.value).to.equal('01.01.2017');
-    });
+    expect(calendar.prop('onChange')).to.equal(wrapper.instance().handleConfirm);
   });
 
   describe('handleConfirm', () => {
     it('calls onConfirm with correct value', () => {
       const onConfirm = simple.mock();
-      const value = '01.01.2017';
+      const value = '12.12.2017';
       const instance = getWrapper({ onConfirm }).instance();
-      instance.state.value = value;
-      instance.handleConfirm();
+      instance.handleConfirm(value);
       expect(onConfirm.callCount).to.equal(1);
       expect(onConfirm.lastCall.args).to.deep.equal([value]);
+    });
+
+    it('calls hideModal', () => {
+      const instance = getWrapper().instance();
+      simple.mock(instance, 'hideModal');
+      instance.handleConfirm();
+      expect(instance.hideModal.callCount).to.equal(1);
+      simple.restore();
+    });
+  });
+
+  describe('hideModal', () => {
+    it('sets state.visible to false', () => {
+      const instance = getWrapper().instance();
+      instance.state.visible = true;
+      instance.hideModal();
+      expect(instance.state.visible).to.be.false;
+    });
+  });
+
+  describe('showModal', () => {
+    it('sets state.visible to true', () => {
+      const instance = getWrapper().instance();
+      instance.state.visible = false;
+      instance.showModal();
+      expect(instance.state.visible).to.be.true;
     });
   });
 });
