393efc1fb3582c6b24f6fade70486e778c0557be
Juha Yrjölä <juha.yrjola@iki.fi>
2018-03-01 10:35:38 +0200

    Update Mapbox URL with new API token

--- app/shared/resource-map/MapContainer.js
+++ app/shared/resource-map/MapContainer.js
@@ -95,7 +95,7 @@ export class UnconnectedResourceMapContainer extends React.Component {
         >
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://api.mapbox.com/styles/v1/city-of-helsinki/cj0w88p4j00qw2rnp9h8ylt7s/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiY2l0eS1vZi1oZWxzaW5raSIsImEiOiJjaXdwMTg1bXowMDBoMnZueHlod2RqbXRyIn0.7FKlCDKY-lDr2BNFbNlQ1w"
+            url="https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoidmFyYWFtbyIsImEiOiJjamU2cWhmdTUwM2ZmMzFud2dvZzFqb2dnIn0.Mj6YrtV9RbJXiU82dqqwhw"
           />
           <ZoomControl position="bottomright" />
           {this.props.markers && this.props.markers.map(
