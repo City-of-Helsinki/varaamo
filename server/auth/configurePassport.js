6b1a9d09d13e145063c4a7b71c37cca72cce5577
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2018-06-04 04:26:31 +0300

    Use other tunnistamo instance

--- server/auth/configurePassport.js
+++ server/auth/configurePassport.js
@@ -4,6 +4,10 @@ import { Strategy } from 'passport-helsinki';
 function configurePassport() {
   const helsinkiStrategy = new Strategy(
     {
+      authorizationURL: 'https://tunnistamo-demo.metatavu.io/oauth2/authorize/',
+      tokenURL: 'https://tunnistamo-demo.metatavu.io/oauth2/token/',
+      userProfileURL: 'https://tunnistamo-demo.metatavu.io/user/',
+      appTokenURL: 'https://tunnistamo-demo.metatavu.io/jwt-token/',
       clientID: process.env.CLIENT_ID,
       clientSecret: process.env.CLIENT_SECRET,
       callbackURL: process.env.LOGIN_CALLBACK_URL || '/login/helsinki/return',
