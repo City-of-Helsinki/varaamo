489f5505bcc7f09159d720fb82fb7be4da219dc2
Riiheläinen Titta <Titta.Riihelainen@digia.com>
2018-06-04 14:51:57 +0300

    Removed unit name and added distance to resource on resource map info. Fixed search control select background color.

--- app/pages/resource/resource-map-info/ResourceMapInfo.js
+++ app/pages/resource/resource-map-info/ResourceMapInfo.js
@@ -2,28 +2,31 @@ import React, { PropTypes } from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import Grid from 'react-bootstrap/lib/Grid';
 import iconMapMarker from 'hel-icons/dist/shapes/map-marker.svg';
-import upperFirst from 'lodash/upperFirst';
+import { round, upperFirst } from 'lodash';
 
 import { injectT } from 'i18n';
 import { getServiceMapUrl } from 'utils/unitUtils';
 
-function getAddress({ addressZip, municipality, name, streetAddress }) {
+function getAddress({ distance }, { addressZip, municipality, streetAddress }) {
+  const km = distance ? distance / 1000 : 0;
+  const formatedDistance = km < 10 ? round(km, 1) : round(km);
+  const distanceStr = km ? `${formatedDistance} km` : '';
   const parts = [
-    name,
+    distanceStr,
     streetAddress,
     `${addressZip} ${upperFirst(municipality)}`.trim(),
   ];
   return parts.filter(part => part).join(', ');
 }
 
-function ResourceMapInfo({ unit, t }) {
+function ResourceMapInfo({ resource, unit, t }) {
   return (
     <div className="app-ResourceMapInfo">
       <Grid>
         {unit &&
           <div className="app-ResourceMapInfo__content">
             <img alt={t('ResourceInfo.serviceMapLink')} src={iconMapMarker} />
-            <span>{getAddress(unit)}</span>
+            <span>{getAddress(resource, unit)}</span>
             <Button
               bsStyle="link"
               href={getServiceMapUrl(unit)}
@@ -38,6 +41,7 @@ function ResourceMapInfo({ unit, t }) {
 }
 
 ResourceMapInfo.propTypes = {
+  resource: PropTypes.object.isRequired,
   t: PropTypes.func.isRequired,
   unit: PropTypes.object.isRequired,
 };
--- app/pages/resource/resource-map-info/ResourceMapInfo.spec.js
+++ app/pages/resource/resource-map-info/ResourceMapInfo.spec.js
@@ -3,17 +3,24 @@ import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import Immutable from 'seamless-immutable';
 
+import Resource from 'utils/fixtures/Resource';
 import Unit from 'utils/fixtures/Unit';
 import { shallowWithIntl } from 'utils/testUtils';
 import ResourceMapInfo from './ResourceMapInfo';
 
 describe('pages/resource/resource-map-info/ResourceMapInfo', () => {
   const defaultProps = {
-    unit: Immutable(Unit.build({ id: 'aaa:123' })),
+    resource: Immutable(Resource.build()),
+    unit: Immutable(Unit.build({
+      addressZip: '12345',
+      id: 'aaa:123',
+      municipality: 'some city',
+      streetAddress: 'Street address 123',
+    })),
   };
 
-  function getWrapper() {
-    return shallowWithIntl(<ResourceMapInfo {...defaultProps} />);
+  function getWrapper(props) {
+    return shallowWithIntl(<ResourceMapInfo {...defaultProps} {...props} />);
   }
 
   it('renders Service map link as a Button with correct url', () => {
@@ -22,5 +29,34 @@ describe('pages/resource/resource-map-info/ResourceMapInfo', () => {
 
     expect(button).to.have.length(1);
     expect(button.prop('href')).to.equal(expected);
+  });
+
+  it('renders address text with distance', () => {
+    const { addressZip, streetAddress } = defaultProps.unit;
+    const resource = Immutable(Resource.build({ distance: 11500 }));
+    const expected = `12 km, ${streetAddress}, ${addressZip} Some city`;
+    const span = getWrapper({ resource }).find('span');
+
+    expect(span).to.have.length(1);
+    expect(span.text()).to.equal(expected);
+  });
+
+  it('renders address text with distance < 10', () => {
+    const { addressZip, streetAddress } = defaultProps.unit;
+    const resource = Immutable(Resource.build({ distance: 1500 }));
+    const expected = `1.5 km, ${streetAddress}, ${addressZip} Some city`;
+    const span = getWrapper({ resource }).find('span');
+
+    expect(span).to.have.length(1);
+    expect(span.text()).to.equal(expected);
+  });
+
+  it('does not render distance if resource has no distance', () => {
+    const { addressZip, streetAddress } = defaultProps.unit;
+    const expected = `${streetAddress}, ${addressZip} Some city`;
+    const span = getWrapper().find('span');
+
+    expect(span).to.have.length(1);
+    expect(span.text()).to.equal(expected);
   });
 });
