0b1faf95e345d251bbb0e4b57e7a27c4dcc21ee3
Petri Mahanen <petri.mahanen@fraktio.fi>
2019-01-15 12:59:47 +0200

    Use moment in UTC time in time slot tests

--- app/utils/fixtures/TimeSlot.js
+++ app/utils/fixtures/TimeSlot.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import { Factory } from 'rosie';
 
-const BASE_DATE = moment().add(2, 'days');
+const BASE_DATE = moment.utc().add(2, 'days');
 
 const TimeSlot = new Factory()
   .sequence('index')
@@ -10,16 +10,28 @@ const TimeSlot = new Factory()
     return `${start}:00-${start + 1}:00`;
   })
   .attr('asISOString', ['index'], (index) => {
-    const start = moment(BASE_DATE).set('hour', (index + 2) % 24).toISOString();
-    const end = moment(BASE_DATE).set('hour', (index + 3) % 24).toISOString();
+    const start = moment
+      .utc(BASE_DATE)
+      .set('hour', (index + 2) % 24)
+      .toISOString();
+    const end = moment
+      .utc(BASE_DATE)
+      .set('hour', (index + 3) % 24)
+      .toISOString();
     return `${start}/${end}`;
   })
-  .attr('start', ['index'], index => (
-    moment(BASE_DATE).set('hour', (index + 2) % 24).toISOString()
-  ))
-  .attr('end', ['index'], index => (
-    moment(BASE_DATE).set('hour', (index + 3) % 24).toISOString()
-  ))
+  .attr('start', ['index'], index =>
+    moment
+      .utc(BASE_DATE)
+      .set('hour', (index + 2) % 24)
+      .toISOString()
+  )
+  .attr('end', ['index'], index =>
+    moment
+      .utc(BASE_DATE)
+      .set('hour', (index + 3) % 24)
+      .toISOString()
+  )
   .attr('reserved', false)
   .attr('resource', 'some-resource-id');
 
