4dddb1650ffb23694cb4aa8c102bb611350764f4
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-02-08 13:57:06 +0200

    Fix unit tests

--- app/state/selectors/__tests__/urlSearchFiltersSelector.spec.js
+++ app/state/selectors/__tests__/urlSearchFiltersSelector.spec.js
@@ -3,33 +3,33 @@ import MockDate from 'mockdate';
 
 import urlSearchFiltersSelector from 'state/selectors/urlSearchFiltersSelector';
 
-function getProps(date = '2015-10-10', start = '08:30') {
-  return {
-    location: {
-      query: {
-        charge: false,
-        date,
-        distance: '',
-        duration: 30,
-        end: '23:30',
-        page: 1,
-        people: '',
-        purpose: 'some-purpose',
-        search: '',
-        start,
-        unit: '',
-        useTimeRange: false,
-      },
-    },
-  };
-}
-
 describe('Selector: urlSearchFiltersSelector', () => {
+  const filters = {
+    charge: false,
+    date: '2015-10-10',
+    distance: '',
+    duration: '30',
+    end: '23:30',
+    page: 1,
+    people: '',
+    purpose: 'some-purpose',
+    search: '',
+    start: '08:30',
+    unit: '',
+    useTimeRange: false,
+  };
+
+  const getProps = (date = filters.date, start = filters.start) => ({
+    location: {
+      search: `?charge=false&date=${date}&distance=&duration=30&end=23:30&page=1&people=&purpose=some-purpose&search=&start=${start}&unit=&useTimeRange=false`,
+    },
+  });
+
   it('returns search filters from the props', () => {
     const state = {};
     const props = getProps();
     const actual = urlSearchFiltersSelector(state, props);
-    const expected = props.location.query;
+    const expected = filters;
 
     expect(actual).to.deep.equal(expected);
   });
@@ -40,7 +40,6 @@ describe('Selector: urlSearchFiltersSelector', () => {
     MockDate.set('2015-12-24T16:07:37Z');
     const actual = urlSearchFiltersSelector(state, props);
     MockDate.reset();
-    const filters = props.location.query;
     const expected = Object.assign({}, filters, { date: '2015-12-24' });
 
     expect(actual).to.deep.equal(expected);
