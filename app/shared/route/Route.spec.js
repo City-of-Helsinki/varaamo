3886c68a4a7ddfb79510bd6a91f4907cbe68035b
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-02-11 16:53:13 +0200

    Unit tests for Route component

---
+++ app/shared/route/Route.spec.js
@@ -0,0 +1,55 @@
+import { expect } from 'chai';
+import React from 'react';
+import simple from 'simple-mock';
+import { shallow } from 'enzyme';
+import { Route as ReactRouterRoute } from 'react-router-dom';
+
+import { Route } from './Route';
+
+describe.only('shared/reservation-controls/ReservationControls', () => {
+  const updateRoute = simple.mock();
+  const scrollToMock = simple.mock();
+
+  beforeEach(() => {
+    updateRoute.reset();
+    scrollToMock.reset();
+    simple.mock(window, 'scrollTo', scrollToMock);
+  });
+
+  const getWrapper = (componentName) => {
+    const props = {
+      updateRoute,
+      componentName,
+    };
+    return shallow(<Route {...props} />);
+  };
+
+  describe('shared/route/Route', () => {
+    it('renders Route from react-router-dom', () => {
+      const wrapper = getWrapper('Homepage');
+
+      expect(wrapper.is(ReactRouterRoute)).to.be.true;
+    });
+
+    it('calls updateRoute when the component did mount', () => {
+      const wrapper = getWrapper('Homepage');
+      wrapper.instance().componentDidMount();
+
+      expect(updateRoute.callCount).to.equal(1);
+    });
+
+    it('calls scrollTo when the component did update', () => {
+      const wrapper = getWrapper('Homepage');
+      wrapper.instance().componentDidMount();
+
+      expect(scrollToMock.callCount).to.equal(1);
+    });
+
+    it('calls updateRoute when the component did update', () => {
+      const wrapper = getWrapper('Homepage');
+      wrapper.instance().componentDidUpdate();
+
+      expect(updateRoute.callCount).to.equal(1);
+    });
+  });
+});
