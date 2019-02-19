c0960b26e822160e55f2c2a201608843c19675d2
Chi Nguyen <chi.nguyen@digia.com>
2019-02-19 11:32:52 +0200

    fix redux-persist and seamless-immuatble

--- app/store/middleware/persistConfig.js
+++ app/store/middleware/persistConfig.js
@@ -1,10 +1,15 @@
 
 import storage from 'redux-persist/lib/storage';
-
+import {
+  seamlessImmutableReconciler,
+  seamlessImmutableTransformCreator,
+} from 'redux-persist-seamless-immutable';
 
 const persistConfig = {
   key: 'root',
   storage,
+  stateReconciler: seamlessImmutableReconciler,
+  transforms: [seamlessImmutableTransformCreator({})],
 };
 
 export default persistConfig;
