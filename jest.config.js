2b440aaceb65af33bb8ec01e2d8b88322c33aae0
Chi Nguyen <chi.nguyen@digia.com>
2019-02-19 21:11:42 +0200

    add jest config

--- jest.config.js
+++ jest.config.js
@@ -1,55 +1,48 @@
-/* eslint-disable max-len */
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-  // Global vars
-  globals: {
-    SETTINGS: true,
-  },
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
+    // Global vars
+    globals: {
+      "SETTINGS": true
+    },
+    // Automatically clear mock calls and instances between every test
+    clearMocks: true,
+  
+    // An array of glob patterns indicating a set of files for which coverage information should be collected
+    collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
+  
+    // The directory where Jest should output its coverage files
+    coverageDirectory: 'coverage',
+  
+    // An array of file extensions your modules use
+    moduleFileExtensions: ['js', 'jsx'],
+  
+    // Same with webpack module resolver
+    moduleDirectories: ["node_modules", "app"],
 
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['src/**/*.{js,jsx,mjs}'],
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
-
-  // An array of file extensions your modules use
-  moduleFileExtensions: ['js', 'jsx'],
-
-  // Mock files
-  moduleNameMapper: {
-    '\\.(css|less|sass|scss)$': '<rootDir>/config/jest/styleMock.js',
-    '\\.(gif|ttf|eot|svg)$': '<rootDir>/config/jest/fileMock.js',
-  },
-
-  // Same with webpack module resolver
-  moduleDirectories: ['node_modules', 'app'],
-
-  // The paths to modules that run some code to configure or set up the testing environment before each test
-  setupFiles: ['<rootDir>config/jest/setupJest.js'],
-
-  // The test environment that will be used for testing
-  testEnvironment: 'jsdom',
-
-  // The glob patterns Jest uses to detect test files
-  testMatch: ['**/?(*.)+(spec).js?(x)'],
-
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
-
-  // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
-  testURL: 'http://localhost',
-
-  // Babel transform
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
-
-  // Indicates whether each individual test should be reported during the run
-  verbose: false,
-};
+    // The paths to modules that run some code to configure or set up the testing environment before each test
+    setupFiles: ['<rootDir>config/setupJest.js'],
+  
+    // The test environment that will be used for testing
+    testEnvironment: 'jsdom',
+  
+    // The glob patterns Jest uses to detect test files
+    testMatch: ['**/?(*.)+(spec).js?(x)'],
+  
+    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+    testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+  
+    // This option sets the URL for the jsdom environment. It is reflected in properties such as location.href
+    testURL: 'http://localhost',
+  
+    // Babel transform 
+    transform: {
+      "^.+\\.jsx?$": "babel-jest"
+    },
+    // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    
+    // Indicates whether each individual test should be reported during the run
+    verbose: false,
+  };
