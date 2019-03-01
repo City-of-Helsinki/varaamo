dcf2c9a999432ea1b9d4e3a5293b82f19bb7a32b
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-03-01 14:36:15 +0200

    Upgrade React to v15.6.2, Enzyme and other related dependencies.

--- config/specs.bootstrap.js
+++ config/specs.bootstrap.js
@@ -1,5 +1,10 @@
 import 'babel-polyfill';
 
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-15';
+
+Enzyme.configure({ adapter: new Adapter(), disableLifecycleMethods: true });
+
 /* eslint-disable no-console */
 console.warning = (...args) => { throw Error(`console.warning: ${args.join(' ')}`); };
 console.error = (...args) => { throw Error(`console.error: ${args.join(' ')}`); };
