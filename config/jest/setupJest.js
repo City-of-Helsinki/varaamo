20090d3db676797a53b4a5e5a17ccbd99c0762bd
Chi Nguyen <chi.nguyen@digia.com>
2019-03-04 17:15:30 +0200

    omit lint in webpack, fix scss import error, add global replacement for mocha

--- config/jest/setupJest.js
+++ config/jest/setupJest.js
@@ -2,12 +2,14 @@ import '@babel/polyfill';
 
 // Enzyme config
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from 'enzyme-adapter-react-15.4';
 
 // fetch() polyfill for making API calls.
 require('isomorphic-fetch');
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter(), disableLifecycleMethods: true });
 
 // mock window, jsdom intergrated with Jest
 window.scrollTo = () => {};
+global.before = beforeAll;
+global.after = afterAll;
