9036edb6e6ab9677a657cad729f473a9290e695b
Chi Nguyen <chi.nguyen@digia.com>
2019-03-05 15:04:32 +0200

    add fi as default locale, add moment-timezone to all test

--- config/jest/setupJest.js
+++ config/jest/setupJest.js
@@ -1,6 +1,6 @@
 import '@babel/polyfill';
-
-// Enzyme config
+import 'moment-timezone';
+import moment from 'moment';
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
 
@@ -8,6 +8,10 @@ import Adapter from 'enzyme-adapter-react-15';
 require('isomorphic-fetch');
 
 configure({ adapter: new Adapter(), disableLifecycleMethods: true });
+
+// Adding global locale for all unit test
+moment.tz.setDefault('Europe/Helsinki');
+moment.locale('fi');
 
 // mock window, jsdom intergrated with Jest
 window.scrollTo = () => {};
