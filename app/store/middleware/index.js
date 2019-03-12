f700917f869e96cf232ce2c9d69c2278fe4e5afb
Chi Nguyen <chi.nguyen@digia.com>
2019-03-12 17:10:10 +0200

    remove persist library with vanilla js

--- app/store/middleware/index.js
+++ app/store/middleware/index.js
@@ -2,14 +2,12 @@ import { applyMiddleware } from 'redux';
 import { apiMiddleware } from 'redux-api-middleware';
 import thunk from 'redux-thunk';
 
-import persistState from './persistState';
 import tracking from './tracking';
 
 const storeEnhancers = [
   applyMiddleware(thunk),
   applyMiddleware(apiMiddleware),
   applyMiddleware(tracking),
-  persistState,
 ];
 
 export default storeEnhancers;
