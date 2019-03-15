d663341ebc7636041467f6cf9b074b0ddfef0bd3
Chi Nguyen <chi.nguyen@digia.com>
2019-03-15 10:08:47 +0200

    fix name props got moved to input of redux-form

--- app/shared/form-fields/TermsField.js
+++ app/shared/form-fields/TermsField.js
@@ -6,13 +6,13 @@ import FormGroup from 'react-bootstrap/lib/FormGroup';
 import HelpBlock from 'react-bootstrap/lib/HelpBlock';
 
 function TermsField({
-  input, label, labelLink, meta, name, onClick
+  input, label, labelLink, meta, onClick
 }) {
   const showError = meta.error && meta.touched;
   return (
     <FormGroup
       className="terms-checkbox-field"
-      controlId={name}
+      controlId={input.name}
       validationState={showError ? 'error' : undefined}
     >
       <Col sm={9}>
@@ -35,7 +35,6 @@ TermsField.propTypes = {
   label: PropTypes.string.isRequired,
   labelLink: PropTypes.string.isRequired,
   meta: PropTypes.object.isRequired,
-  name: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
 };
 
