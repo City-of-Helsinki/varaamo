4b1aad85490393ca7bd2e2b9898e952e9a510e34
Chi Nguyen <chi.nguyen@digia.com>
2019-03-18 10:08:51 +0200

    fix wrong imports

--- app/shared/comment-form/CommentForm.spec.js
+++ app/shared/comment-form/CommentForm.spec.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDom from 'react-dom';
 import Button from 'react-bootstrap/lib/Button';
 import FormControl from 'react-bootstrap/lib/FormControl';
 import simple from 'simple-mock';
