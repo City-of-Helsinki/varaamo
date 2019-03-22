14582c08796602091a64cb87f79731ba57635277
Chi Nguyen <chi.nguyen@digia.com>
2019-03-22 16:28:31 +0200

    Refactor ResourceCard with reuseable cell, add favorite icon

--- app/shared/resource-card/ResourceCardContainer.js
+++ app/shared/resource-card/ResourceCardContainer.js
@@ -1,6 +1,17 @@
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import resourceCardSelector from './resourceCardSelector';
 import ResourceCard from './ResourceCard';
+import { favoriteResource, unfavoriteResource } from 'actions/resourceActions';
 
-export default connect(resourceCardSelector)(ResourceCard);
+function mapDispatchToProps(dispatch) {
+  const actionCreators = {
+    favoriteResource,
+    unfavoriteResource,
+  };
+
+  return { actions: bindActionCreators(actionCreators, dispatch) };
+}
+
+export default connect(resourceCardSelector, mapDispatchToProps)(ResourceCard);
