7a6301ce2afba33a3b73a717c9f55f01fdcf3530
Rasbin Rijal <rasbin.rijal@gmail.com>
2019-04-03 16:20:25 +0300

    Change inline styles to styling in SASS

--- app/index.js
+++ app/index.js
@@ -23,37 +23,30 @@ const finalState = Immutable(initialStoreState).merge([initialServerState, initi
   deep: true,
 });
 const store = configureStore(finalState);
-const warningStyle = {
-  margin: '20px',
-  padding: '20px',
-  color: '#5d3d08',
-  backgroundColor: '#fcf8e3',
-  border: '2px solid #faebcc',
-};
 
 if (browserName === 'IE') {
   render(
     <div>
-      <p style={warningStyle}>
+      <p className="warningStyle">
         Currently, Varaamo does not support IE11.
         We are investigating this issue and finding a solution.
         Meanwhile, use another browser (such as
-        <a href="https://www.google.com/chrome/" style={{ textDecoration: 'none' }}> Chrome</a>
+        <a href="https://www.google.com/chrome/"> Chrome</a>
         ,
-        <a href="https://www.mozilla.org/en-US/firefox/new/" style={{ textDecoration: 'none' }}> Firefox </a>
+        <a href="https://www.mozilla.org/en-US/firefox/new/"> Firefox </a>
         or
-        <a href="https://www.microsoft.com/en-us/windows/microsoft-edge" style={{ textDecoration: 'none' }}> Edge</a>
+        <a href="https://www.microsoft.com/en-us/windows/microsoft-edge"> Edge</a>
         ).
       </p>
-      <p style={warningStyle}>
+      <p className="warningStyle">
         Varaamo ei tue IE11 selainta tällä hetkellä.
         Selvitämme ongelmaa sen ratkaisemiseksi.
         Sillävälin, käytä toista selainta (kuten
-        <a href="https://www.google.com/chrome/" style={{ textDecoration: 'none' }}> Chrome</a>
+        <a href="https://www.google.com/chrome/"> Chrome</a>
         ,
-        <a href="https://www.mozilla.org/en-US/firefox/new/" style={{ textDecoration: 'none' }}> Firefox </a>
+        <a href="https://www.mozilla.org/en-US/firefox/new/"> Firefox </a>
         tai
-        <a href="https://www.microsoft.com/en-us/windows/microsoft-edge" style={{ textDecoration: 'none' }}> Edge</a>
+        <a href="https://www.microsoft.com/en-us/windows/microsoft-edge"> Edge</a>
         ).
       </p>
     </div>,
