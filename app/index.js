911045745a69eeb6bdd5dc38e33d46c8671b9442
Rasbin Rijal <rasbin.rijal@gmail.com>
2019-04-03 14:38:58 +0300

    Show warning message for IE users

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
+import 'react-app-polyfill/ie11';
+import { browserName } from 'react-device-detect';
 import 'location-origin';
-
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-intl-redux';
@@ -22,10 +23,47 @@ const finalState = Immutable(initialStoreState).merge([initialServerState, initi
   deep: true,
 });
 const store = configureStore(finalState);
+const warningStyle = {
+  margin: '20px',
+  padding: '20px',
+  color: '#5d3d08',
+  backgroundColor: '#fcf8e3',
+  border: '2px solid #faebcc',
+};
 
-render(
-  <Provider store={store}>
-    <Router>{getRoutes()}</Router>
-  </Provider>,
-  document.getElementById('root')
-);
+if (browserName === 'IE') {
+  render(
+    <div>
+      <p style={warningStyle}>
+        Currently, Varaamo does not support IE11.
+        We are investigating this issue and finding a solution.
+        Meanwhile, use another browser (such as
+        <a href="https://www.google.com/chrome/" style={{ textDecoration: 'none' }}> Chrome</a>
+        ,
+        <a href="https://www.mozilla.org/en-US/firefox/new/" style={{ textDecoration: 'none' }}> Firefox </a>
+        or
+        <a href="https://www.microsoft.com/en-us/windows/microsoft-edge" style={{ textDecoration: 'none' }}> Edge</a>
+        ).
+      </p>
+      <p style={warningStyle}>
+        Varaamo ei tue IE11 selainta tällä hetkellä.
+        Selvitämme ongelmaa sen ratkaisemiseksi.
+        Sillävälin, käytä toista selainta (kuten
+        <a href="https://www.google.com/chrome/" style={{ textDecoration: 'none' }}> Chrome</a>
+        ,
+        <a href="https://www.mozilla.org/en-US/firefox/new/" style={{ textDecoration: 'none' }}> Firefox </a>
+        tai
+        <a href="https://www.microsoft.com/en-us/windows/microsoft-edge" style={{ textDecoration: 'none' }}> Edge</a>
+        ).
+      </p>
+    </div>,
+    document.getElementById('root')
+  );
+} else {
+  render(
+    <Provider store={store}>
+      <Router>{getRoutes()}</Router>
+    </Provider>,
+    document.getElementById('root')
+  );
+}
