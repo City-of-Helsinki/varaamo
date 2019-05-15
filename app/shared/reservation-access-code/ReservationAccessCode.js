3ea2c65c9088d0e339eea38171ff4ea005a5654c
Jimi <jiripim@gmail.com>
2019-05-15 17:26:31 +0300

    Replace default export with wrapper component

---
+++ app/shared/reservation-access-code/ReservationAccessCode.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import PendingAccessCode from './PendingAccessCode';
+import GeneratedAccessCode from './GeneratedAccessCode';
+import { isAccessCodeGenerated, isAccessCodePending } from './helpers';
+
+const ReservationAccessCode = ({ reservation, resource, text }) => {
+  if (isAccessCodeGenerated(reservation)) {
+    return <GeneratedAccessCode reservation={reservation} text={text} />;
+  }
+  if (resource && isAccessCodePending(reservation, resource)) {
+    return <PendingAccessCode />;
+  }
+  return null;
+};
+
+ReservationAccessCode.propTypes = {
+  reservation: PropTypes.object.isRequired,
+  resource: PropTypes.object,
+  text: PropTypes.string,
+};
+
+export default ReservationAccessCode;
---
+++ app/shared/reservation-access-code/helpers.js
@@ -0,0 +1,11 @@
+import has from 'lodash/has';
+
+export const isAccessCodeGenerated = reservation => has(reservation, 'accessCode');
+
+// eslint-disable-next-line arrow-body-style
+export const isAccessCodePending = (reservation, resource) => {
+  // resource.generateAccessCodes:
+  // true => respa generates an access_code for reservation (immediately)
+  // false => an access_code will be generated for reservation 24h before it starts
+  return !isAccessCodeGenerated(reservation) && resource.generateAccessCodes === false;
+};
