cbf629d784fe7a891558b47a38ee012df4673e5c
Simo Jokela <rboost@gmail.com>
2019-05-28 15:02:28 +0300

    Change import paths to relative for files in ./app/shared/favorite-button/ folder

--- app/shared/favorite-button/FavoriteButton.spec.js
+++ app/shared/favorite-button/FavoriteButton.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from 'react-bootstrap/lib/Button';
 import simple from 'simple-mock';
 
-import { shallowWithIntl } from 'utils/testUtils';
+import { shallowWithIntl } from '../../utils/testUtils';
 import FavoriteButton from './FavoriteButton';
 
 describe('shared/favorite-button/FavoriteButton', () => {
