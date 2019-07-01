c3e665a401a62c240a9288ea9f3a854288bf0462
Rasbin Rijal <rasbin.rijal@gmail.com>
2019-07-01 11:10:22 +0300

    Revert previous commit of removing isRequired attribute, add it back

--- app/pages/resource/resource-info/ResourceInfo.js
+++ app/pages/resource/resource-info/ResourceInfo.js
@@ -70,16 +70,12 @@ function ResourceInfo({
   );
 }
 
-// TODO : Add isRequired in currentEquipments propTypes later when data updated
-// Currently, there are not these data in all resources and causes error
-// So, it is removed for now.
-
 ResourceInfo.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
   resource: PropTypes.object.isRequired,
   t: PropTypes.func.isRequired,
   unit: PropTypes.object.isRequired,
-  currentEquipments: PropTypes.array
+  currentEquipments: PropTypes.array,
 };
 
 ResourceInfo = injectT(ResourceInfo); // eslint-disable-line
