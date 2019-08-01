47a45366f5ebfd32e5ed7abc50704bcaa51188c7
Simo Jokela <rboost@gmail.com>
2019-08-01 12:34:18 +0300

    Add test fixtures for the data the new api client returns

---
+++ src/common/data/fixtures/utils.js
@@ -0,0 +1,9 @@
+export const getLocalizedFieldSequenceGeneratorFunction = (fieldValue, locales = ['en', 'fi', 'sv']) => {
+  return (i) => {
+    const value = {};
+    locales.forEach((locale) => {
+      value[locale] = `${fieldValue} (${locale}) - ${i}`;
+    });
+    return value;
+  };
+};
