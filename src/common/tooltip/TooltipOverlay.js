99b2ebbf0cca7be5c03f61f618b2018a79086cb1
Chi Nguyen <chi.nguyen@digia.com>
2019-08-07 17:06:22 +0300

    add pincode component

---
+++ src/common/tooltip/TooltipOverlay.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { OverlayTrigger, Tooltip as BTTooltip } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+
+
+const TooltipOverlay = ({
+  children, content, placement, ...rest
+}) => {
+  return (
+    <div className="app-TooltipOverlay">
+      <OverlayTrigger
+        overlay={(
+          <BTTooltip id={`tooltip-${placement}`}>
+            {content}
+          </BTTooltip>
+)}
+        placement={placement || 'top'}
+        {...rest}
+      >
+        {children}
+      </OverlayTrigger>
+    </div>
+  );
+};
+
+TooltipOverlay.propTypes = {
+  children: PropTypes.element.isRequired,
+  content: PropTypes.element.isRequired,
+  placement: PropTypes.string
+};
+
+export default TooltipOverlay;
---
+++ src/domain/reservation/manage/pincode/ManageReservationPincode.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+
+import { isAccessCodeGenerated, isAccessCodePending } from '../../../../../app/shared/reservation-access-code/helpers';
+import TooltipOverlay from '../../../../common/tooltip/TooltipOverlay';
+import IconClock from '../../../../../app/assets/icons/clock-o.svg';
+
+class ManageReservationPincode extends Component {
+  renderPincodeField = () => {
+    const { resource, reservation } = this.props;
+    if (isAccessCodeGenerated(reservation)) {
+      return (
+        <TooltipOverlay
+          content={<span>{reservation.accessCode}</span>}
+        >
+          <span>****</span>
+        </TooltipOverlay>
+      );
+    }
+    if (isAccessCodePending(reservation, resource)) {
+      return (
+        <TooltipOverlay
+          content={<FormattedMessage id="ReservationAccessCode.pending" />}
+        >
+          <IconClock />
+        </TooltipOverlay>
+      );
+    }
+
+    return '';
+  }
+
+  render() {
+    return (
+      <div className="app-ManageReservationPincode">
+        {this.renderPincodeField()}
+      </div>
+    );
+  }
+}
+
+ManageReservationPincode.propTypes = {
+  resource: PropTypes.object.isRequired,
+  reservation: PropTypes.object.isRequired
+};
+
+export default ManageReservationPincode;
