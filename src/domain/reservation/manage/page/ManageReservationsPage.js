9ac27b178d8b26a8561e33f4be618f98128bf4f8
Nizar Rahme <nizar.w.rahmah@gmail.com>
2019-08-26 10:33:00 +0300

    Remove unused variable

--- src/domain/reservation/manage/page/ManageReservationsPage.js
+++ src/domain/reservation/manage/page/ManageReservationsPage.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import get from 'lodash/get';
-import pick from 'lodash/pick';
 import Loader from 'react-loader';
 import { withRouter } from 'react-router-dom';
 import { Grid, Row, Col } from 'react-bootstrap';
