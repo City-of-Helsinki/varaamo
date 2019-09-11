01a92a6929aa420137aa57c5570ab5bc96d6dcb3
Bernt Andreas Drange <bernt.andreas.drange@digia.com>
2019-09-11 13:59:47 +0300

    Refactor footer, add snapshot test for footer
    * Move FooterContent into Footer
    * Get rid of unused onLinkClick
    * Get rid of duplicate code
    * Re-enable some tests

--- src/domain/footer/Footer.js
+++ src/domain/footer/Footer.js
@@ -1,18 +1,62 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { FormattedHTMLMessage } from 'react-intl';
+import Col from 'react-bootstrap/lib/Col';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
+import { Link } from 'react-router-dom';
 
-import FooterContent from './FooterContent';
+import { version } from '../../../package.json';
+import FeedbackLink from '../../../app/shared/feedback-link/FeedbackLink';
+import Logo from '../../../app/shared/logo/Logo';
+import injectT from '../../../app/i18n/injectT';
+import { getCurrentCustomization } from '../../../app/utils/customizationUtils';
 
-function Footer({ onLinkClick }) {
+function Footer({ t }) {
+  const feedbackLink = <FeedbackLink>{t('Footer.feedbackLink')}</FeedbackLink>;
+  const customization = {};
+  switch (getCurrentCustomization()) {
+    case 'ESPOO':
+      customization.FormattedHTMLMessageId = 'Footer.espooText';
+      break;
+    case 'VANTAA':
+      customization.FormattedHTMLMessageId = 'Footer.vantaaText';
+      break;
+    case 'HELSINKI':
+    default:
+      customization.FormattedHTMLMessageId = 'Footer.helsinkiText';
+  }
+
   return (
     <footer>
-      <FooterContent onLinkClick={onLinkClick} />
+      <Grid>
+        <Row>
+          <Col lg={3} md={3} xs={12}>
+            <Link className="brand-link" to="/">
+              <Logo />
+              Varaamo
+            </Link>
+          </Col>
+          <Col lg={6} md={6} xs={12}>
+            <p>
+              <FormattedHTMLMessage id={customization.FormattedHTMLMessageId} />
+            </p>
+            <p>
+              {feedbackLink}
+            </p>
+          </Col>
+          <Col lg={3} md={3} xs={12}>
+            <div className="app-varaamo-version-wrapper">
+              <span className="app-varaamo-version">{`v${version}`}</span>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
     </footer>
   );
 }
-
 Footer.propTypes = {
-  onLinkClick: PropTypes.func,
+  t: PropTypes.func.isRequired,
 };
 
-export default Footer;
+export default injectT(Footer);
--- src/domain/footer/__tests__/Footer.test.js
+++ src/domain/footer/__tests__/Footer.test.js
@@ -1,27 +1,89 @@
-import { shallow } from 'enzyme';
 import React from 'react';
+import toJSON from 'enzyme-to-json';
+import simple from 'simple-mock';
 
-import FooterContent from '../FooterContent';
+import { shallowWithIntl } from '../../../../app/utils/testUtils';
+import FeedbackLink from '../../../../app/shared/feedback-link/FeedbackLink';
+import * as customizationUtils from '../../../../app/utils/customizationUtils';
 import Footer from '../Footer';
 
-describe('shared/footer/Footer', () => {
+describe('domain/footer/Footer', () => {
   function getWrapper(props) {
-    return shallow(<Footer {...props} />);
+    return shallowWithIntl(<Footer {...props} />);
   }
 
   test('renders a footer element', () => {
     const footer = getWrapper().find('footer');
     expect(footer.length).toBe(1);
   });
-
-  test('renders FooterContent component', () => {
-    const footerContent = getWrapper().find(FooterContent);
-    expect(footerContent.length).toBe(1);
+  test('renders correctly', () => {
+    const wrapper = shallowWithIntl(
+      <Footer />
+    );
+    expect(toJSON(wrapper)).toMatchSnapshot();
   });
 
-  test('passes onLinkClick prop to FooterContent', () => {
-    const onLinkClick = () => {};
-    const footerContent = getWrapper({ onLinkClick }).find(FooterContent);
-    expect(footerContent.prop('onLinkClick')).toBe(onLinkClick);
+  describe('When there is no customization in use', () => {
+    let content;
+
+    beforeAll(() => {
+      content = getWrapper();
+    });
+
+    test('contains feedback link', () => {
+      const feedbackLink = content.find(FeedbackLink);
+      expect(feedbackLink.length).toBe(1);
+    });
+
+    test('renders correctly', () => {
+      const wrapper = shallowWithIntl(
+        <Footer />
+      );
+      expect(toJSON(wrapper)).toMatchSnapshot();
+    });
+
+    it('renders texts for default (Helsinki)', () => {
+      expect(content.find({ id: 'Footer.helsinkiText' }).length).toBe(1);
+    });
+  });
+
+  describe('When Espoo customization is used', () => {
+    let content;
+
+    beforeAll(() => {
+      simple.mock(customizationUtils, 'getCurrentCustomization').returnWith('ESPOO');
+      content = getWrapper();
+    });
+
+    afterAll(() => {
+      simple.restore();
+    });
+
+    test('contains feedback link', () => {
+      const feedbackLink = content.find(FeedbackLink);
+      expect(feedbackLink.length).toBe(1);
+    });
+
+    it('renders texts for Espoo', () => {
+      expect(content.find({ id: 'Footer.espooText' }).length).toBe(1);
+    });
+  });
+
+  describe('When Vantaa customization is used', () => {
+    let content;
+
+    beforeAll(() => {
+      simple.mock(customizationUtils, 'getCurrentCustomization').returnWith('VANTAA');
+      content = getWrapper();
+    });
+
+    afterAll(() => {
+      simple.restore();
+    });
+
+    test('contains feedback link', () => {
+      const feedbackLink = content.find(FeedbackLink);
+      expect(feedbackLink.length).toBe(1);
+    });
   });
 });
