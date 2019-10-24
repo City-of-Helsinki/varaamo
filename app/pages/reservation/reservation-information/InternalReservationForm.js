06367dd85e3514180ff65d3991dddfc8b87bb76c
Kimmo Virtanen <kimmo.virtanen@phz.fi>
2019-10-24 19:46:07 +0300

    Connect store and dispatch form initial action.

--- app/pages/reservation/reservation-information/InternalReservationForm.js
+++ app/pages/reservation/reservation-information/InternalReservationForm.js
@@ -5,6 +5,7 @@ import Form from 'react-bootstrap/lib/Form';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 import injectT from '../../../i18n/injectT';
 import FormTypes from '../../../constants/FormTypes';
@@ -19,6 +20,23 @@ class UnconnectedInternalReservationForm extends Component {
     internalReservationDefaultChecked: true
   }
 
+  componentDidMount() {
+    this.props.dispatch(this.internalReservationFormInitialValue());
+  }
+
+  internalReservationFormInitialValue() {
+    return {
+      type: '@@redux-form/CHANGE',
+      meta: {
+        form: 'RESERVATION',
+        field: 'internalReservation',
+        touch: false,
+        persistentSubmitErrors: false
+      },
+      payload: this.state.internalReservationDefaultChecked
+    };
+  }
+
   render() {
     const { t } = this.props;
     /**
@@ -27,7 +45,8 @@ class UnconnectedInternalReservationForm extends Component {
      */
     const premiseStaffOnly = 'Premise staff only';
     const internalReservation = 'Internal reservation';
-    const internalReservationDescription = 'The Department\'s own event. Uncheck this box to make an external reservation on behalf of the user.';
+    const internalReservationDescription = `The Department's own event. 
+    Uncheck this box to make an external reservation on behalf of the user.`;
     const markAsClosed = 'Mark as closed';
     const markAsClosedDescription = 'Check if you are closing the resource by reserving.';
     const comment = 'Comment';
@@ -41,7 +60,9 @@ class UnconnectedInternalReservationForm extends Component {
                 checked={this.state.internalReservationDefaultChecked}
                 component="input"
                 name="internalReservation"
-                onChange={() => this.setState(prevState => ({ internalReservationDefaultChecked: !prevState.internalReservationDefaultChecked }))}
+                onChange={() => this.setState(
+                  prevState => ({ internalReservationDefaultChecked: !prevState.internalReservationDefaultChecked })
+                )}
                 type="checkbox"
               />
             </Col>
@@ -89,8 +110,11 @@ class UnconnectedInternalReservationForm extends Component {
   }
 }
 
+connect()(UnconnectedInternalReservationForm);
+
 UnconnectedInternalReservationForm.propTypes = {
   t: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired
 };
 UnconnectedInternalReservationForm = injectT(UnconnectedInternalReservationForm);  // eslint-disable-line
 
