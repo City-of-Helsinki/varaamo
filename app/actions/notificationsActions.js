5d2b8385b3d1bc1b5f059f7848965ec5a8d288f1
Kimmo Virtanen <kimmo.virtanen@phz.fi>
2019-11-07 13:04:20 +0200

    Revert "Refactoring notification code"
    This reverts commit 336a2fe5a224686f6da1cd8271ea167ee8d01148.

---
+++ app/actions/notificationsActions.js
@@ -0,0 +1,13 @@
+import { createAction } from 'redux-actions';
+
+import types from '../constants/ActionTypes';
+
+
+const addNotification = createAction(types.UI.ADD_NOTIFICATION);
+
+const hideNotification = createAction(types.UI.HIDE_NOTIFICATION);
+
+export {
+  addNotification,
+  hideNotification,
+};
