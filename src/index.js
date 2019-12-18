d9adcc181f10819d4f12cf1786004771ec0c2e35
Santtu <santtu.tuovinen2@gmail.com>
2019-12-18 09:29:10 +0200

    Firebase api keys are now in .env, added defaults to .env.example

--- src/index.js
+++ src/index.js
@@ -17,8 +17,20 @@ import BrowserWarning from '../app/pages/browser-warning/BrowserWarning';
 
 const isIEBrowser = browserName === 'IE';
 
-const firebaseConfig = {};
-firebase.initializeApp(firebaseConfig);
+/* eslint-disable no-undef */
+// Initialize firebase project from .env variables
+if (FIREBASE.API_KEY && FIREBASE.API_KEY.length > 0) {
+  firebase.initializeApp({
+    apiKey: FIREBASE.API_KEY,
+    authDomain: FIREBASE.AUTH_DOMAIN,
+    databaseURL: FIREBASE.DATABASE_URL,
+    projectId: FIREBASE.PROJECT_ID,
+    storageBucket: FIREBASE.STORAGE_BUCKET,
+    messagingSenderId: FIREBASE.MESSAGING_SENDER_ID,
+    appId: FIREBASE.APP_ID,
+    measurementId: FIREBASE.MEASUREMENT_ID
+  });
+}
 
 // TODO: Support IE11 in the future.
 render(
