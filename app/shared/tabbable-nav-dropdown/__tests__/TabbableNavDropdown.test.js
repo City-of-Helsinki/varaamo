8d383af8f746b2e083f92f45183cc4819fc89592
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-01-29 15:29:54 +0200

    Make language and sign out menus accessible with tab
    Previously the menus did not support te tab key, but supported arrow
    keys instead. This may be confusing to keyboard users who need to
    change from using the tab key into using the navigation keys.
    
    Unfortunately, react-boostrap's navigation dropdown is built to ignore
    the tab key so I needed to implement a new component. The new component
    uses all the old styles to the best of my knowledge.

---
+++ app/shared/tabbable-nav-dropdown/__tests__/TabbableNavDropdown.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import TabbableNavDropdown from '../TabbableNavDropdown';
+
+// Testing keyboard functionality here would be nice, but as far as I understand
+// Enzyme offers poor support for events bound to the document, because it
+// focuses on React.
+//
+// Similarly, jsdom does not implement tabbing, so we would need a cypress test
+// to verify tabbing behaviour.
+describe('<TabbableNavDropdown />', () => {
+  const renderToggle = props => <div {...props}>test</div>;
+  const defaultProps = {
+    renderToggle,
+    children: () => {},
+  };
+  const getWrapper = props => shallow(<TabbableNavDropdown {...defaultProps} {...props} />);
+  const getWrappingEl = wrapper => wrapper.dive().first();
+
+  it('should allow for its wrapping element to be set', () => {
+    expect(getWrappingEl(getWrapper()).name()).toEqual('div');
+    expect(getWrappingEl(getWrapper({ as: 'li' })).name()).toEqual('li');
+  });
+
+  // The state update fails to take place even when the event handler is fired.
+  // There's some Enzyme quirk here I don't understand.
+  it.skip('should set open class on wrapping element when open', () => {
+    const wrapper = getWrapper();
+
+    wrapper.find({ children: 'test' }).simulate('click', { preventDefault: () => {} });
+
+    expect(getWrappingEl(wrapper).hasClass('open')).toEqual(true);
+  });
+});
---
+++ app/shared/tabbable-nav-dropdown/__tests__/TabbableNavItem.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import TabbableNavItem from '../TabbableNavItem';
+
+describe('<TabbableNavItem />', () => {
+  const defaultProps = {
+  };
+  const getWrapper = props => shallow(<TabbableNavItem {...defaultProps} {...props} />);
+  const getTargetableEl = wrapper => wrapper.children().first().dive();
+
+  it('should allow for its targetable element\'s type to be set to either a or button', () => {
+    expect(getTargetableEl(getWrapper()).name()).toEqual('a');
+    expect(getTargetableEl(getWrapper({ as: 'button' })).name()).toEqual('button');
+  });
+
+  it('should render null for other element types than button or a', () => {
+    const originalError = global.console.error;
+    global.console.error = () => {};
+
+    expect(getWrapper({ as: 'div' }).type()).toEqual(null);
+
+    global.console.error = originalError;
+  });
+
+  it('should spread other props than as on targetable element', () => {
+    const testChildren = 'Content';
+
+    expect(getTargetableEl(getWrapper({ children: testChildren })).prop('children')).toEqual(testChildren);
+  });
+
+  describe('when a button', () => {
+    it('should set type attribute', () => {
+      expect(getTargetableEl(getWrapper({ as: 'button' })).prop('type')).toEqual('button');
+    });
+  });
+});
