7eea15ebc39f987729e1632cb4b5ce05d5201e78
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-06-12 11:01:24 +0300

    Toggle font size selector off with a feature flag

---
+++ src/domain/feature-flags/getIsFeatureEnabled.js
@@ -0,0 +1,18 @@
+import FeatureFlags from './FeatureFlags';
+
+/**
+ * getIsFeatureEnabled();
+ * Returns true when feature is enabled.
+ * @param flagName {string}
+ * @returns {boolean}
+ */
+function getIsFeatureEnabled(flagName) {
+  switch (flagName) {
+    case FeatureFlags.FONT_SIZE_CONTROLS:
+      return false;
+    default:
+      return false;
+  }
+}
+
+export default getIsFeatureEnabled;
