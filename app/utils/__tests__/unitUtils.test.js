950aee63c732f92d49ba938b2b8830cb05bb9c95
Jaakko Jokinen <jaakko.jokinen@teamit.fi>
2020-06-16 12:41:18 +0300

    Fix service map link

--- app/utils/__tests__/unitUtils.test.js
+++ app/utils/__tests__/unitUtils.test.js
@@ -6,7 +6,7 @@ describe('Utils: unitUtils', () => {
   describe('getServiceMapUrl', () => {
     test('returns correct url', () => {
       const unit = { id: 'abc:123' };
-      const expected = 'https://palvelukartta.hel.fi/unit/123#!route-details';
+      const expected = 'https://palvelukartta.hel.fi/unit/123';
 
       expect(getServiceMapUrl(unit)).toBe(expected);
     });
