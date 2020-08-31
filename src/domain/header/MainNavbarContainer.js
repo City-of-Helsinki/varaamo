2e2f0f0ce35f98a0223d666c7b5dc6e46b673d5b
Nizar Rahme <nizar.w.rahmah@gmail.com>
2020-08-31 13:46:05 +0300

    Fetch accessibility viewpoints

--- src/domain/header/MainNavbarContainer.js
+++ src/domain/header/MainNavbarContainer.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
@@ -5,6 +6,27 @@ import { clearSearchResults } from '../../../app/actions/searchActions';
 import { isAdminSelector, isLoggedInSelector } from '../../../app/state/selectors/authSelectors';
 import changeLocale from '../../../app/i18n/changeLocale';
 import MainNavbar from './MainNavbar';
+import { ApiClient } from '../../common/api/client';
+import settings from '../../../config/settings';
+
+const apiClient = new ApiClient(settings.ACCESSIBILITY_API_URL, false);
+
+const MainNavbarContainer = (props) => {
+  const [viewpoints, setViewpoints] = useState([]);
+
+  useEffect(() => {
+    apiClient.get('viewpoints').then(
+      ({ error, data }) => {
+        if (error || viewpoints.length) return;
+        if (Array.isArray(data)) setViewpoints(data);
+      },
+    );
+  });
+
+  return (
+    <MainNavbar {...props} viewpoints={viewpoints} />
+  );
+};
 
 export const selector = createStructuredSelector({
   isAdmin: isAdminSelector,
@@ -16,4 +38,4 @@ const actions = {
   clearSearchResults,
 };
 
-export default connect(selector, actions)(MainNavbar);
+export default connect(selector, actions)(MainNavbarContainer);
